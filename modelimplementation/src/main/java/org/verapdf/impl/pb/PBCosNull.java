1b722554bc63aa85f06f296e0ccd8786e4d7924f
Evgeniy Muravitskiy <evgeniy.muravitskiy@duallab.com>
2015-05-07 12:25:06 +0300

    Add support of Integer, Name, Null, Number, Object implementations

---
+++ modelimplementation/src/main/java/org/verapdf/impl/pb/PBCosNull.java
@@ -0,0 +1,27 @@
+package org.verapdf.impl.pb;
+
+import org.apache.pdfbox.cos.COSNull;
+import org.verapdf.model.coslayer.CosNull;
+
+/**
+ * Created by Evgeniy Muravitskiy on 4/28/15.
+ * <p>
+ *     Current class is representation of CosNull interface of abstract model.
+ *     This class is analogue of COSNull in pdfbox.
+ * </p>
+ */
+public final class PBCosNull extends PBCosObject implements CosNull {
+
+    /** PDF null object
+     */
+    public final static CosNull NULL = new PBCosNull(COSNull.NULL);
+
+    private PBCosNull(COSNull nil) {
+        super(nil);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return o instanceof CosNull;
+    }
+}
