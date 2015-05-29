e69b3940a5590dba4bbe24f9938e24da2fe4b668
Evgeniy Muravitskiy <evgeniy.muravitskiy@duallab.com>
2015-05-29 11:33:20 +0300

    Implement new properties for PBCosStream

--- modelimplementation/src/main/java/org/verapdf/model/impl/pb/cos/PBCosStream.java
+++ modelimplementation/src/main/java/org/verapdf/model/impl/pb/cos/PBCosStream.java
@@ -7,10 +7,9 @@ import org.verapdf.model.coslayer.CosStream;
 import java.io.IOException;
 
 /**
- * Created by Evgeniy Muravitskiy on 5/4/15.
- * <p>
- *     PDF Stream type
- * </p>
+ * PDF Stream type
+ *
+ * @author Evgeniy Mutavitskiy
  */
 public class PBCosStream extends PBCosDict implements CosStream {
 
@@ -24,23 +23,9 @@ public class PBCosStream extends PBCosDict implements CosStream {
     /**  length of the stream
      */
     @Override
-    public Long getlength() {
-        Long length = 0L;
-        COSStream stream = (COSStream) this.baseObject;
-
-        if (stream.getItem(COSName.LENGTH) != null)
-            return (long) ((COSInteger) stream.getItem(COSName.LENGTH)).intValue();
-
-        try {
-            if (stream.getUnfilteredStream() != null)
-                length = (long) (stream.getUnfilteredStream()).available();
-            if (stream.getFilteredStream() != null)
-                length += stream.getFilteredLength();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return length;
+    public Long getLength() {
+        COSNumber number = (COSNumber) ((COSStream) this.baseObject).getItem(COSName.LENGTH);
+        return number != null ? number.longValue() : null;
     }
 
     /**  concatenated (white space separated) names of all filters
@@ -69,11 +54,32 @@ public class PBCosStream extends PBCosDict implements CosStream {
         return filters.substring(0, filters.length() - 2);
     }
 
+    public String getF() {
+        return null;
+    }
+
+    public String getFFilter() {
+        return null;
+    }
+
+    public String getFDecodeParms() {
+        return null;
+    }
+
     /**  true if the spacing around stream / endstream complies with the PDF/A requirements
      */
     @Override
-    public Boolean getspacingComplyPDFA() {
+    public Boolean getspacingCompliesPDFA() {
         logger.warn("Current feature not supported yet. Result is always true.");
         return true;
+    }
+
+    /** true if the value of Length key matches the actual length of the stream
+     */
+    @Override
+    public Boolean getisLengthCorrect() {
+        Long keyValue = getLength();
+        Long realValue = ((COSStream) baseObject).getOriginLength();
+        return keyValue != null && keyValue.equals(realValue);
     }
 }
