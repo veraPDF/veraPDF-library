b10c069b709c87b93ade48d54b43f2b3cd908258
Evgeniy Muravitskiy <evgeniy.muravitskiy@duallab.com>
2015-05-29 11:34:02 +0300

    Add support of new properties for CosTrailer

--- modelimplementation/src/main/java/org/verapdf/model/impl/pb/cos/PBCosTrailer.java
+++ modelimplementation/src/main/java/org/verapdf/model/impl/pb/cos/PBCosTrailer.java
@@ -1,6 +1,8 @@
 package org.verapdf.model.impl.pb.cos;
 
+import org.apache.pdfbox.cos.COSArray;
 import org.apache.pdfbox.cos.COSDictionary;
+import org.apache.pdfbox.cos.COSString;
 import org.verapdf.model.coslayer.CosTrailer;
 
 /**
@@ -11,8 +13,59 @@ import org.verapdf.model.coslayer.CosTrailer;
  */
 public class PBCosTrailer extends PBCosDict implements CosTrailer {
 
+    private COSDictionary linearizedDictionary = null;
+    private CosTrailer firstTrailer = null;
+    private Long length = null;
+
     public PBCosTrailer(COSDictionary pdfBoxObject) {
         super(pdfBoxObject);
         setType("CosTrailer");
+    }
+
+    public PBCosTrailer(COSDictionary mainTrailer, COSDictionary firstTrailer, COSDictionary linearizedDictionary, Long length) {
+        super(mainTrailer);
+        this.firstTrailer = new PBCosTrailer(firstTrailer);
+        this.linearizedDictionary = linearizedDictionary;
+        this.length = length;
+    }
+
+    public String getid1() {
+        return getPartOfID(0);
+    }
+
+    public String getid2() {
+        return getPartOfID(1);
+    }
+
+    private String getPartOfID(int i) {
+        COSArray ids = (COSArray) ((COSDictionary) baseObject).getItem("ID");
+
+        if (ids != null && ids.size() > i) {
+            if (ids.get(i) instanceof COSString) {
+                return new String(((COSString) ids.get(i)).getBytes());
+            } else {
+                // TODO : discuss about this case
+                throw new IllegalArgumentException("ID not conforming defined type for ID.");
+            }
+        } else {
+            return null;
+        }
+    }
+
+    // TODO : need to support of this feature
+    public Boolean getisLinearized() {
+        if (baseObject == firstTrailer || linearizedDictionary == null) {
+            return false;
+        }
+        return true;
+    }
+
+    public Boolean getisEncrypted() {
+        return ((COSDictionary) baseObject).getItem("Encrypt") != null;
+    }
+
+    @Override
+    public Boolean getdoFirstLastTrailerIDsMatch() {
+        return getid1().equals(firstTrailer.getid1()) && getid2().equals(firstTrailer.getid2());
     }
 }
