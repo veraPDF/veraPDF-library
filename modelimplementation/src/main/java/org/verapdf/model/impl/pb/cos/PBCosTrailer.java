57d7ecd71aff70398fff05e3bafe400b2f07d91d
Evgeniy Muravitskiy <evgeniy.muravitskiy@duallab.com>
2015-06-01 13:20:13 +0300

    Add link for catalog from trailer. Add javadoc for PBCosTrailer

--- modelimplementation/src/main/java/org/verapdf/model/impl/pb/cos/PBCosTrailer.java
+++ modelimplementation/src/main/java/org/verapdf/model/impl/pb/cos/PBCosTrailer.java
@@ -3,18 +3,30 @@ package org.verapdf.model.impl.pb.cos;
 import org.apache.pdfbox.cos.COSArray;
 import org.apache.pdfbox.cos.COSDictionary;
 import org.apache.pdfbox.cos.COSString;
+import org.verapdf.model.baselayer.*;
+import org.verapdf.model.baselayer.Object;
+import org.verapdf.model.coslayer.CosIndirect;
 import org.verapdf.model.coslayer.CosTrailer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Created by Evgeniy Muravitskiy on 5/1/15.
- * <p>
- *     Trailer of the document. It has the same behavior as the PBCosDict.
- * </p>
+ * Trailer of the document.
+ *
+ * @author Evgeniy Muravitskiy
  */
 public class PBCosTrailer extends PBCosDict implements CosTrailer {
 
+    private final String CATALOG = "Catalog";
+    /** if document is linearized its must have dictionary of linearization
+     */
     private COSDictionary linearizedDictionary = null;
+    /** if document is linearized first trailer is differ from last
+     */
     private CosTrailer firstTrailer = null;
+    /** length of the document
+     */
     private Long length = null;
 
     public PBCosTrailer(COSDictionary pdfBoxObject) {
@@ -29,10 +41,16 @@ public class PBCosTrailer extends PBCosDict implements CosTrailer {
         this.length = length;
     }
 
+    /**
+     * @return first part of ID if its present
+     */
     public String getid1() {
         return getPartOfID(0);
     }
 
+    /**
+     * @return second part of ID if its present
+     */
     public String getid2() {
         return getPartOfID(1);
     }
@@ -52,17 +70,46 @@ public class PBCosTrailer extends PBCosDict implements CosTrailer {
         }
     }
 
+    /**
+     * @return true if the current document is linearized
+     */
     // TODO : need to support of this feature
     public Boolean getisLinearized() {
         return !(baseObject == firstTrailer || linearizedDictionary == null);
     }
 
+    /**
+     * @return true if the current document is encrypted
+     */
     public Boolean getisEncrypted() {
         return ((COSDictionary) baseObject).getItem("Encrypt") != null;
     }
 
+    /**
+     * @return true if ID of first and last trailers are match
+     */
     @Override
     public Boolean getdoFirstLastTrailerIDsMatch() {
         return getid1().equals(firstTrailer.getid1()) && getid2().equals(firstTrailer.getid2());
+    }
+
+    @Override
+    public List<? extends Object> getLinkedObjects(String link) {
+        List<? extends Object> list;
+        switch (link) {
+            case CATALOG:
+                list = getCatalog();
+                break;
+            default:
+                list = super.getLinkedObjects(link);
+                break;
+        }
+        return list;
+    }
+
+    private List<CosIndirect> getCatalog() {
+        List<CosIndirect> catalog = new ArrayList<>(1);
+        catalog.add((CosIndirect) ((COSDictionary) baseObject).getItem(CATALOG));
+        return catalog;
     }
 }
