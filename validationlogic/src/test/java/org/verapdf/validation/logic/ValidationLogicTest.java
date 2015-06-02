c30bf398ab788aae69d79b0b09d6acd2aa74832c
Carl Wilson <carl@openpreservation.org>
2015-06-02 19:06:05 +0100

    Unchecked cast supressed.

--- validationlogic/src/test/java/org/verapdf/validation/logic/ValidationLogicTest.java
+++ validationlogic/src/test/java/org/verapdf/validation/logic/ValidationLogicTest.java
@@ -4,8 +4,8 @@ import java.net.URISyntaxException;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import org.junit.Test;
 
+import org.junit.Test;
 import org.verapdf.model.baselayer.Object;
 import org.verapdf.model.coslayer.CosDict;
 import org.verapdf.model.coslayer.CosInteger;
@@ -19,53 +19,56 @@ import static org.junit.Assert.*;
  */
 public class ValidationLogicTest {
 
-    @Test
-    public void test() throws Exception {
+	@SuppressWarnings("unchecked")
+	@Test
+	public void test() throws Exception {
 
-        TestPDAnnot pda1 = new TestPDAnnot("pdatestFailedVersion","pda1");
-        TestPDAnnot pda2 = new TestPDAnnot("pdatest","pda2");
-        List<PDAnnot> lspd = new ArrayList<>();
-        lspd.add(pda1);
-        lspd.add(pda2);
+		TestPDAnnot pda1 = new TestPDAnnot("pdatestFailedVersion", "pda1");
+		TestPDAnnot pda2 = new TestPDAnnot("pdatest", "pda2");
+		List<PDAnnot> lspd = new ArrayList<>();
+		lspd.add(pda1);
+		lspd.add(pda2);
 
-        TestCosInteger in = new TestCosInteger(41, null);
-        List<CosInteger> lsin = new ArrayList<>();
-        lsin.add(in);
+		TestCosInteger in = new TestCosInteger(41, null);
+		List<CosInteger> lsin = new ArrayList<>();
+		lsin.add(in);
 
-        TestCosDict cd2 = new TestCosDict(2, "cd2", new ArrayList<CosDict>(), lsin, new ArrayList<org.verapdf.model.baselayer.Object>());
+		TestCosDict cd2 = new TestCosDict(2, "cd2", new ArrayList<CosDict>(),
+				lsin, new ArrayList<org.verapdf.model.baselayer.Object>());
 
-        List<CosDict> lscd = new ArrayList<>();
-        lscd.add(cd2);
+		List<CosDict> lscd = new ArrayList<>();
+		lscd.add(cd2);
 
-        TestCosDict cd1 = new TestCosDict(3, "cd1", lscd, lsin, new ArrayList<org.verapdf.model.baselayer.Object>());
+		TestCosDict cd1 = new TestCosDict(3, "cd1", lscd, lsin,
+				new ArrayList<org.verapdf.model.baselayer.Object>());
 
+		List<CosDict> lscd2 = new ArrayList<>();
+		lscd2.add(cd1);
 
-        List<CosDict> lscd2 = new ArrayList<>();
-        lscd2.add(cd1);
+		TestObject obj = new TestObject("obj", lscd2, lspd);
 
-        TestObject obj = new TestObject("obj", lscd2, lspd);
+		((List<Object>) cd2.getLinkedObjects("Object")).add(obj);
 
+		ValidationInfo info = Validator.validate(obj,
+				getSystemIndependentPath("/test.xml"));
 
-        ((List<Object>) cd2.getLinkedObjects("Object")).add(obj);
+		assertEquals(info.getProfile().getName(),
+				"Validation profile for testing");
+		assertEquals(info.getProfile().getHash(), "Some hash");
 
+		assertFalse(info.getResult().isCompliant());
 
-        ValidationInfo info = Validator.validate(obj, getSystemIndependentPath("/test.xml"));
+		assertEquals(info.getResult().getSummary().getAttrPassedRules(), 2);
+		assertEquals(info.getResult().getSummary().getAttrFailedRules(), 3);
 
-        assertEquals(info.getProfile().getName(), "Validation profile for testing");
-        assertEquals(info.getProfile().getHash(), "Some hash");
+		assertEquals(info.getResult().getSummary().getAttrPassedChecks(), 6);
+		assertEquals(info.getResult().getSummary().getAttrFailedChecks(), 4);
 
-        assertFalse(info.getResult().isCompliant());
+	}
 
-        assertEquals(info.getResult().getSummary().getAttrPassedRules(), 2);
-        assertEquals(info.getResult().getSummary().getAttrFailedRules(), 3);
-
-        assertEquals(info.getResult().getSummary().getAttrPassedChecks(), 6);
-        assertEquals(info.getResult().getSummary().getAttrFailedChecks(), 4);
-
-    }
-
-    private String getSystemIndependentPath(String path) throws URISyntaxException {
-        return Paths.get(this.getClass().getResource(path).toURI()).toString();
-    }
+	private String getSystemIndependentPath(String path)
+			throws URISyntaxException {
+		return Paths.get(this.getClass().getResource(path).toURI()).toString();
+	}
 
 }
