82322815f502bc032d8f4e99b72678a3c62e7ba9
Timur Kamalov <timur.kamalov@duallab.com>
2015-06-03 15:13:01 +0300

    IT for isartor 6.1.12 clause.

---
+++ core/src/test/java/org/verapdf/integration/pdfa_1/b_6_1_12_implementation_limits/Isartor_6_1_12_t01_fail_b_IT.java
@@ -0,0 +1,31 @@
+package org.verapdf.integration.pdfa_1.b_6_1_12_implementation_limits;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.verapdf.integration.base.BasePDFAIT;
+
+/**
+ * Implementation limits - Name length
+ */
+public class Isartor_6_1_12_t01_fail_b_IT extends BasePDFAIT {
+
+    @Test
+    public void testValidation() throws Exception {
+        Assert.assertTrue(testValidationSuccessful());
+    }
+
+    @Override
+    protected String getExpectedReportFilePath() throws Exception {
+        return getSystemIndependentPath(ISARTOR_EXPECTED_REPORTS_REPO_NAME + "pdfa_1/b_6_1_12_implementation_limits/6-1-12-t01-fail-b-report.xml");
+    }
+
+    @Override
+    protected String getValidationProfileFilePath() throws Exception {
+        return getSystemIndependentPath(VALIDATION_PROFILES_REPO_NAME + "PDF_A/1b/6.1 File structure/6.1.12 Implementation limits/verapdf-profile-6-1-12-t04.xml");
+    }
+
+    @Override
+    protected String getPdfFilePath() throws Exception {
+        return getSystemIndependentPath(ISARTOR_TEST_FILES_REPO_NAME + "PDFA-1b/6.1 File structure/6.1.12 Implementation Limits/isartor-6-1-12-t01-fail-b.pdf");
+    }
+}
