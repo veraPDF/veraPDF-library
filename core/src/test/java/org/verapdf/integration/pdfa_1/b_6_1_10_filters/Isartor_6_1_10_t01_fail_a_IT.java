3be941f94ac0e05aa434338b1cb0a8aff1319cdc
Timur Kamalov <timur.kamalov@duallab.com>
2015-06-03 14:44:07 +0300

    IT for isartor 6.1.10 clause.

---
+++ core/src/test/java/org/verapdf/integration/pdfa_1/b_6_1_10_filters/Isartor_6_1_10_t01_fail_a_IT.java
@@ -0,0 +1,31 @@
+package org.verapdf.integration.pdfa_1.b_6_1_10_filters;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.verapdf.integration.base.BasePDFAIT;
+
+/**
+ * Filters - LZW decode
+ */
+public class Isartor_6_1_10_t01_fail_a_IT extends BasePDFAIT {
+
+    @Test
+    public void testValidation() throws Exception {
+        Assert.assertTrue(testValidationSuccessful());
+    }
+
+    @Override
+    protected String getExpectedReportFilePath() throws Exception {
+        return getSystemIndependentPath(ISARTOR_EXPECTED_REPORTS_REPO_NAME + "pdfa_1/b_6_1_10_filters/6-1-10-t01-fail-a-report.xml");
+    }
+
+    @Override
+    protected String getValidationProfileFilePath() throws Exception {
+        return getSystemIndependentPath(VALIDATION_PROFILES_REPO_NAME + "PDF_A/1b/6.1 File structure/6.1.10 Filters/verapdf-profile-6-1-10-t01.xml");
+    }
+
+    @Override
+    protected String getPdfFilePath() throws Exception {
+        return getSystemIndependentPath(ISARTOR_TEST_FILES_REPO_NAME + "PDFA-1b/6.1 File structure/6.1.10 Filters/isartor-6-1-10-t01-fail-a.pdf");
+    }
+}
