0e0aef2d1ac0d24e6b3217c32ae086b9eeeed530
Evgeniy Muravitskiy <evgeniy.muravitskiy@duallab.com>
2015-07-01 12:27:01 +0300

    Add unit tests for cos layer

---
+++ modelimplementation/src/test/java/org/verapdf/model/impl/pb/cos/PBCosDocumentTest.java
@@ -0,0 +1,168 @@
+package org.verapdf.model.impl.pb.cos;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.junit.*;
+import org.verapdf.model.baselayer.*;
+import org.verapdf.model.baselayer.Object;
+import org.verapdf.model.coslayer.CosDocument;
+import org.verapdf.model.coslayer.CosTrailer;
+import org.verapdf.model.coslayer.CosXRef;
+import org.verapdf.model.impl.BaseTest;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+/**
+ * @author Evgeniy Muravitskiy
+ */
+public class PBCosDocumentTest extends BaseTest {
+
+    private static final String FILE_RELATIVE_PATH = "/model/impl/pb/cos/veraPDF test suite 6-1-2-t02-fail-a.pdf";
+
+    private static final Long expectedNumberOfIndirects = Long.valueOf(17);
+    private static final Long expectedSizeOfDocument = Long.valueOf(9437);
+    private static final String[] expectedIDS = new String[]{"D6CF927DCF82444068EB69A5914F8070",
+            "A2A7539F7C71DEBB6A4A6B418235962D"};
+
+
+    @BeforeClass
+    public static void setUp() throws URISyntaxException, IOException {
+        TYPE = "CosDocument";
+        ID = null;
+
+        String fileAbsolutePath = getSystemIndependentPath(FILE_RELATIVE_PATH);
+        final File file = new File(fileAbsolutePath);
+        try (PDDocument document = PDDocument.load(file, Boolean.FALSE, Boolean.TRUE)) {
+            actual = new PBCosDocument(document, file.length());
+        }
+    }
+
+    private static String getSystemIndependentPath(String path) throws URISyntaxException {
+        URL resourceUrl = ClassLoader.class.getResource(path);
+        Path resourcePath = Paths.get(resourceUrl.toURI());
+        return resourcePath.toString();
+    }
+
+    @Test
+    public void testNumberOfIndirectsMethod() {
+        Assert.assertEquals(expectedNumberOfIndirects, ((CosDocument) actual).getnrIndirects());
+    }
+
+    @Test
+    public void testSizeMethod() {
+        Assert.assertEquals(expectedSizeOfDocument, ((CosDocument) actual).getsize());
+    }
+
+    @Test
+    public void testBinaryHeaderMethod() {
+        Assert.assertFalse(((CosDocument) actual).getbinaryHeaderComplyPDFA());
+    }
+
+    @Test
+    public void testPDFHeaderMethod() {
+        Assert.assertTrue(((CosDocument) actual).getpdfHeaderCompliesPDFA());
+    }
+
+    @Test
+    public void testOptionalContentMethod() {
+        Assert.assertFalse(((CosDocument) actual).getisOptionalContentPresent());
+    }
+
+    @Test
+    public void testEOFMethod() {
+        Assert.assertTrue(((CosDocument) actual).geteofCompliesPDFA());
+    }
+
+    @Test
+    public void testFirstPageTrailer() {
+        String[] actualIDs = ((CosDocument) actual).getfirstPageID().split(" ");
+        Assert.assertEquals(expectedIDS.length, actualIDs.length);
+        for (int index = 0; index < actualIDs.length; index++) {
+            Assert.assertEquals(getExpectedID(index), actualIDs[index]);
+        }
+    }
+
+    @Test
+    public void testLastTrailer() {
+        String[] actualIDs = ((CosDocument) actual).getlastID().split(" ");
+        Assert.assertEquals(expectedIDS.length, actualIDs.length);
+        for (int index = 0; index < actualIDs.length; index++) {
+            Assert.assertEquals(getExpectedID(index), actualIDs[index]);
+        }
+    }
+
+    // problems with code symbols
+    private String getExpectedID(int index) {
+        StringBuilder builder = new StringBuilder(16);
+        for (int i = 0; i < expectedIDS[index].length(); i += 2) {
+            builder.append((char) Integer.parseInt(expectedIDS[index].substring(i, i + 2), 16));
+        }
+        return builder.toString();
+    }
+
+    @Test
+    public void testIsLinearized() {
+        Assert.assertFalse(((CosDocument) actual).getisLinearized());
+    }
+
+    @Test
+    public void testInfoMatchXMP() {
+        Assert.assertTrue(((CosDocument) actual).getdoesInfoMatchXMP());
+    }
+
+    @Test
+    public void testTrailerLink() {
+        List<? extends Object> trailer = actual.getLinkedObjects(PBCosDocument.TRAILER);
+        Assert.assertEquals(1, trailer.size());
+        Assert.assertTrue(trailer.get(0) != null && trailer.get(0) instanceof CosTrailer);
+    }
+
+    @Test
+    public void testIndirectObjectsLink() {
+        List<? extends Object> indirects = actual.getLinkedObjects(PBCosDocument.INDIRECT_OBJECTS);
+        Assert.assertEquals(expectedNumberOfIndirects.intValue(), indirects.size());
+        for (Object indirect : indirects) {
+            Assert.assertTrue(indirect != null);
+            Assert.assertTrue(indirect instanceof PBCosIndirect);
+        }
+    }
+
+    @Test
+    public void testDocumentLink() {
+        List<? extends Object> document = actual.getLinkedObjects(PBCosDocument.DOCUMENT);
+        Assert.assertEquals(1, document.size());
+        Assert.assertTrue(document.get(0) != null);
+        Assert.assertTrue(document.get(0) instanceof org.verapdf.model.pdlayer.PDDocument);
+    }
+
+    @Test
+    public void testXREFLink() {
+        List<? extends Object> xref = actual.getLinkedObjects(PBCosDocument.XREF);
+        Assert.assertEquals(1, xref.size());
+        Assert.assertTrue(xref.get(0) != null);
+        Assert.assertTrue(xref.get(0) instanceof CosXRef);
+    }
+
+    @Test
+    public void testEmbeddedFilesLink() {
+        List<? extends Object> embeddedFiles = actual.getLinkedObjects(PBCosDocument.EMBEDDED_FILES);
+        Assert.assertEquals(0, embeddedFiles.size());
+    }
+
+    @Test(expected = IllegalAccessError.class)
+    public void testNonexistentParentLink() {
+        actual.getLinkedObjects("Wrong link.");
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        TYPE = null;
+        ID = null;
+        actual = null;
+    }
+}
---
+++ modelimplementation/src/test/java/org/verapdf/model/impl/pb/cos/PBCosStreamTest.java
@@ -0,0 +1,116 @@
+package org.verapdf.model.impl.pb.cos;
+
+import org.apache.pdfbox.cos.*;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.verapdf.model.baselayer.Object;
+import org.verapdf.model.coslayer.CosStream;
+import org.verapdf.model.impl.BaseTest;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * @author Evgeniy Muravitskiy
+ */
+public class PBCosStreamTest extends BaseTest {
+
+    private static final Long expectedOriginLength = Long.valueOf(10);
+    private static String[] expectedFiltersArray;
+    public static final String expectedFFilterName = COSName.ASCII85_DECODE.getName();
+
+    @BeforeClass
+    public static void setUp() throws IOException {
+        TYPE = "CosStream";
+        ID = null;
+
+        COSStream stream = getCosStream();
+
+        actual = new PBCosStream(stream);
+    }
+
+    private static COSStream getCosStream() throws IOException {
+        COSArray array = new COSArray();
+        expectedFiltersArray = new String[2];
+
+        array.add(COSName.LZW_DECODE);
+        array.add(COSName.FLATE_DECODE);
+
+        expectedFiltersArray[0] = COSName.LZW_DECODE.getName();
+        expectedFiltersArray[1] = COSName.FLATE_DECODE.getName();
+
+        COSObject object = new COSObject(new COSDictionary());
+        object.setObjectNumber(Long.valueOf(10));
+        object.setGenerationNumber(Integer.valueOf(0));
+
+        COSStream stream = new COSStream(new COSDictionary());
+        stream.setOriginLength(expectedOriginLength);
+        stream.setInt(COSName.LENGTH, expectedOriginLength.intValue() - 1);
+        stream.setItem(COSName.FILTER, array);
+        stream.setItem(COSName.F_FILTER, COSName.ASCII85_DECODE);
+        stream.setItem(COSName.F_DECODE_PARMS, object);
+        return stream;
+    }
+
+    @Test
+    public void testGetLengthMethod() {
+        Assert.assertEquals(expectedOriginLength - 1, ((CosStream) actual).getLength().longValue());
+    }
+
+    @Test
+    public void testGetFiltersMethod() {
+        String[] actualFilters = ((CosStream) actual).getfilters().split(" ");
+        Assert.assertEquals(expectedFiltersArray.length, actualFilters.length);
+        for (int index = 0; index < actualFilters.length; index++) {
+            Assert.assertEquals(expectedFiltersArray[index], actualFilters[index]);
+        }
+    }
+
+    @Test
+    public void testGetFMethod() {
+        Assert.assertTrue(((CosStream) actual).getF() == null);
+    }
+
+    @Test
+    public void testFFilterMethod() {
+        Assert.assertEquals(expectedFFilterName, ((CosStream) actual).getFFilter());
+    }
+
+    @Test
+    public void testFDecodeParmsMethod() {
+        Assert.assertTrue(((CosStream) actual).getFDecodeParms() != null);
+    }
+
+    @Test
+    public void testGetSpacingCompliesPDFAMethod() {
+        Assert.assertTrue(((CosStream) actual).getspacingCompliesPDFA());
+    }
+
+    @Test
+    public void testGetIsLengthCorrect() {
+        Assert.assertFalse(((CosStream) actual).getisLengthCorrect());
+    }
+
+    @Test
+    public void testParentLink() {
+        final List<? extends Object> parentLink = actual.getLinkedObjects(PBCosDict.METADATA);
+        Assert.assertEquals(parentLink.size(), 0);
+    }
+
+    @Test(expected = IllegalAccessError.class)
+    public void testNonexistentParentLink() {
+        actual.getLinkedObjects("Wrong link.");
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        TYPE = null;
+        actual = null;
+        for (int i = 0; i < expectedFiltersArray.length; i++) {
+            expectedFiltersArray[i] = null;
+        }
+        expectedFiltersArray = null;
+    }
+}
