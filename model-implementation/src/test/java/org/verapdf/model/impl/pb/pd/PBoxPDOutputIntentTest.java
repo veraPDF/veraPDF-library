e2be9723ec56cd78e8a397aa568bd03abbfdcbd2
Evgeniy Muravitskiy <evgeniy.muravitskiy@duallab.com>
2015-07-20 12:10:22 +0300

    Add output intent test

---
+++ model-implementation/src/test/java/org/verapdf/model/impl/pb/pd/PBoxPDOutputIntentTest.java
@@ -0,0 +1,54 @@
+package org.verapdf.model.impl.pb.pd;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.verapdf.model.baselayer.Object;
+import org.verapdf.model.impl.BaseTest;
+import org.verapdf.model.impl.pb.external.PBoxICCOutputProfile;
+import org.verapdf.model.pdlayer.PDOutputIntent;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
+
+/**
+ * @author Evgeniy Muravitskiy
+ */
+public class PBoxPDOutputIntentTest extends BaseTest {
+
+	public static final String FILE_RELATIVE_PATH = "cos/veraPDF test suite 6-1-2-t02-fail-a.pdf";
+
+	@BeforeClass
+	public static void setUp() throws URISyntaxException, IOException {
+		expectedType = "PDOutputIntent";
+		expectedID = null;
+
+		setUp(FILE_RELATIVE_PATH);
+		actual = new PBoxPDOutputIntent(document.getDocumentCatalog().getOutputIntents().get(0));
+	}
+
+	@Test
+	public void testDestOutputProfileRefMethod() {
+		Assert.assertEquals("7 0", ((PDOutputIntent) actual).getdestOutputProfileRef());
+	}
+
+	@Test
+	public void testDestProfileLink() {
+		List<? extends org.verapdf.model.baselayer.Object> destProfile = actual.getLinkedObjects("destProfile");
+		Assert.assertEquals(1, destProfile.size());
+		for (Object object : destProfile) {
+			Assert.assertEquals(PBoxICCOutputProfile.ICC_OUTPUT_PROFILE_TYPE, object.getType());
+		}
+	}
+
+	@AfterClass
+	public static void tearDown() throws IOException {
+		expectedType = null;
+		expectedID = null;
+		actual = null;
+
+		document.close();
+	}
+}
