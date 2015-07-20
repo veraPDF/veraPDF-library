2d087434c36fe5a8225d59177d9bd31628f259cb
Evgeniy Muravitskiy <evgeniy.muravitskiy@duallab.com>
2015-07-20 17:16:51 +0300

    Add test for annotations

---
+++ model-implementation/src/test/java/org/verapdf/model/impl/pb/pd/PBoxPDAnnotTest.java
@@ -0,0 +1,106 @@
+package org.verapdf.model.impl.pb.pd;
+
+import org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotation;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.verapdf.model.baselayer.Object;
+import org.verapdf.model.impl.BaseTest;
+import org.verapdf.model.pdlayer.PDAnnot;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
+
+/**
+ * @author Evgeniy Muravitskiy
+ */
+public class PBoxPDAnnotTest extends BaseTest {
+
+	public static final String FILE_RELATIVE_PATH = "pd/InteractiveObjects.pdf";
+
+	@BeforeClass
+	public static void setUp() throws URISyntaxException, IOException {
+		expectedType = PBoxPDAnnot.ANNOTATION_TYPE;
+		expectedID = null;
+
+		setUp(FILE_RELATIVE_PATH);
+		List<PDAnnotation> annotations = document.getPage(0).getAnnotations();
+		actual = new PBoxPDAnnot(annotations.get(annotations.size() - 1));
+	}
+
+	@Test
+	public void testSubtypeMethod() {
+		Assert.assertEquals("Widget", ((PDAnnot) actual).getSubtype());
+	}
+
+	@Test
+	public void testAPMethod() {
+		Assert.assertNotNull(((PDAnnot) actual).getAP());
+	}
+
+	@Test
+	public void testFMethod() {
+		Assert.assertEquals(Long.valueOf(4), ((PDAnnot) actual).getF());
+	}
+
+	@Test
+	public void testCAMethod() {
+		Assert.assertNull(((PDAnnot) actual).getCA());
+	}
+
+	@Test
+	public void testAdditionalActionLink() {
+		List<? extends Object> action = actual.getLinkedObjects(PBoxPDAnnot.ADDITIONAL_ACTION);
+		Assert.assertEquals(0, action.size());
+		for (Object object : action) {
+			Assert.assertTrue("PDAction".equals(object.getType()) || "PDNamedAction".equals(object.getType()));
+		}
+	}
+
+	@Test
+	public void testActionLink() {
+		List<? extends Object> action = actual.getLinkedObjects(PBoxPDAnnot.A);
+		Assert.assertEquals(1, action.size());
+		for (Object object : action) {
+			Assert.assertTrue("PDAction".equals(object.getType()) || "PDNamedAction".equals(object.getType()));
+		}
+	}
+
+	@Test
+	public void testICLink() {
+		List<? extends Object> action = actual.getLinkedObjects(PBoxPDAnnot.IC);
+		Assert.assertEquals(0, action.size());
+		for (Object object : action) {
+			Assert.assertEquals("CosReal", object.getType());
+		}
+	}
+
+	@Test
+	public void testCLink() {
+		List<? extends Object> action = actual.getLinkedObjects(PBoxPDAnnot.C);
+		Assert.assertEquals(0, action.size());
+		for (Object object : action) {
+			Assert.assertEquals("CosReal", object.getType());
+		}
+	}
+
+	@Test
+	public void testAppearanceLink() {
+		List<? extends Object> action = actual.getLinkedObjects(PBoxPDAnnot.APPEARANCE);
+		Assert.assertEquals(1, action.size());
+		for (Object object : action) {
+			Assert.assertEquals("PDContentStream", object.getType());
+		}
+	}
+
+	@AfterClass
+	public static void tearDown() throws IOException {
+		expectedType = null;
+		expectedID = null;
+		actual = null;
+
+		document.close();
+	}
+}
