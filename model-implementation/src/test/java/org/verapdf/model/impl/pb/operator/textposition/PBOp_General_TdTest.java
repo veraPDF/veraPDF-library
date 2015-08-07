c0a792e1c978e11c5e6fe3c182ffb507d2f15cc4
Evgeniy Muravitskiy <evgeniy.muravitskiy@duallab.com>
2015-08-07 17:22:49 +0300

    Add unit tests for text position operators

---
+++ model-implementation/src/test/java/org/verapdf/model/impl/pb/operator/textposition/PBOp_General_TdTest.java
@@ -0,0 +1,34 @@
+package org.verapdf.model.impl.pb.operator.textposition;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.verapdf.model.baselayer.Object;
+import org.verapdf.model.coslayer.CosReal;
+import org.verapdf.model.impl.pb.cos.PBCosReal;
+import org.verapdf.model.impl.pb.operator.base.PBOperatorTest;
+
+import java.util.List;
+
+/**
+ * @author Evgeniy Muravitskiy
+ */
+public abstract class PBOp_General_TdTest extends PBOperatorTest {
+
+	protected static Long expectedVertical;
+	protected static Long expectedHorizontal;
+
+	@Test
+	public void testVerticalOffset() {
+		testOffset(PBOp_General_Td.VERTICAL_OFFSET, expectedVertical);
+	}
+
+	@Test
+	public void testHorizontalOffset() {
+		testOffset(PBOp_General_Td.HORIZONTAL_OFFSET, expectedHorizontal);
+	}
+
+	private void testOffset(String link, Long value) {
+		Object object = getObject(link, 1, PBCosReal.COS_REAL_TYPE);
+		Assert.assertEquals(value, ((CosReal) object).getintValue());
+	}
+}
