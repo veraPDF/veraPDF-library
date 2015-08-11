c097859135e457f97026a664ae48f692a96abc13
Evgeniy Muravitskiy <evgeniy.muravitskiy@duallab.com>
2015-08-11 17:00:11 +0300

    Fixes in color space operators

--- model-implementation/src/main/java/org/verapdf/model/impl/pb/operator/color/PBOpColor.java
+++ model-implementation/src/main/java/org/verapdf/model/impl/pb/operator/color/PBOpColor.java
@@ -7,6 +7,9 @@ import org.verapdf.model.operator.OpColor;
 import java.util.List;
 
 /**
+ * Base class for pdf color operators like CS, cs, SC, SCN,
+ * sc, scn, G, g, RG, rg, K, k
+ *
  * @author Timur Kamalov
  */
 public class PBOpColor extends PBOperator implements OpColor {
