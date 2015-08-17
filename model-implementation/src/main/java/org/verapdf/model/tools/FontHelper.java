a0fcba17f83312d9b46b3374d29949683181667c
Evgeniy Muravitskiy <evgeniy.muravitskiy@duallab.com>
2015-08-17 15:34:27 +0300

    Rewrite FontHelper

--- model-implementation/src/main/java/org/verapdf/model/tools/FontHelper.java
+++ model-implementation/src/main/java/org/verapdf/model/tools/FontHelper.java
@@ -1,31 +1,46 @@
 package org.verapdf.model.tools;
 
+import org.apache.pdfbox.cos.COSName;
 import org.apache.pdfbox.pdmodel.font.*;
 import org.apache.pdfbox.preflight.font.container.*;
+import org.verapdf.model.factory.font.FontFactory;
 
 /**
  * @author Timur Kamalov
  */
 public class FontHelper {
 
+	public static final String CID_FONT_TYPE_0 = "CIDFontType0";
+	public static final String CID_FONT_TYPE_2 = "CIDFontType2";
+
 	public static FontContainer getFontContainer(PDFont font) {
-		if (font instanceof PDTrueTypeFont) {
-			return new TrueTypeContainer((PDTrueTypeFont) font);
-		} else if (font instanceof PDType1Font) {
-			return new Type1Container((PDType1Font) font);
-		} else if (font instanceof PDType3Font) {
-			return new Type3Container((PDType3Font) font);
-		} else if (font instanceof PDType0Font) {
-			Type0Container container = new Type0Container(font);
-			org.apache.pdfbox.pdmodel.font.PDCIDFont pdcidFont = ((PDType0Font) font).getDescendantFont();
-			if (pdcidFont instanceof PDCIDFontType0) {
-				container.setDelegateFontContainer(new CIDType0Container((PDCIDFontType0) pdcidFont));
-			} else if (pdcidFont instanceof PDCIDFontType2) {
-				container.setDelegateFontContainer(new CIDType2Container((PDCIDFontType2) pdcidFont));
+		switch (font.getSubType()) {
+			case FontFactory.TYPE_1:
+			case FontFactory.TYPE_1C:
+				return new Type1Container((PDSimpleFont) font);
+			case FontFactory.TRUE_TYPE:
+				return new TrueTypeContainer((PDTrueTypeFont) font);
+			case FontFactory.TYPE_3:
+				return new Type3Container((PDType3Font) font);
+			case FontFactory.TYPE_0: {
+				Type0Container container = new Type0Container(font);
+				PDCIDFont pdcidFont = ((PDType0Font) font)
+						.getDescendantFont();
+				String cidType = pdcidFont.getCOSObject()
+						.getNameAsString(COSName.SUBTYPE);
+				if (CID_FONT_TYPE_0.equals(cidType)) {
+					CIDType0Container type0Container =
+							new CIDType0Container((PDCIDFontType0) pdcidFont);
+					container.setDelegateFontContainer(type0Container);
+				} else if (CID_FONT_TYPE_2.equals(cidType)) {
+					CIDType2Container type2Container =
+							new CIDType2Container((PDCIDFontType2) pdcidFont);
+					container.setDelegateFontContainer(type2Container);
+				}
 			}
-			return container;
+			default:
+				return null;
 		}
-		return null;
 	}
 
 }
