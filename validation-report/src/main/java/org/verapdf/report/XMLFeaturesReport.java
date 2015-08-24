98f45dfc31710951a0c26efa0523deb6ff9beb16
Maksim Bezrukov <maksim.bezrukov@duallab.com>
2015-08-24 09:52:58 +0300

    Metadata features report fixes.

--- validation-report/src/main/java/org/verapdf/report/XMLFeaturesReport.java
+++ validation-report/src/main/java/org/verapdf/report/XMLFeaturesReport.java
@@ -8,6 +8,7 @@ import org.verapdf.features.tools.FeaturesCollection;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
+import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -15,7 +16,6 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.nio.charset.Charset;
 import java.util.Arrays;
 import java.util.Formatter;
 import java.util.List;
@@ -39,36 +39,46 @@ public final class XMLFeaturesReport {
     private static final int LF = 0xA;
     private static final int CR = 0xD;
 
-    private static final byte[] utf8MetadataPrefix = {0x3C, 0x3F, 0x78, 0x70, 0x61, 0x63, 0x6B,
-            0x65, 0x74, 0x20, 0x62, 0x65, 0x67, 0x69, 0x6E, 0x3D};
-    private static final byte[] utf16BEMetadataPrefix = {0x00, 0x3C, 0x00, 0x3F, 0x00, 0x78, 0x00, 0x70, 0x00, 0x61, 0x00,
-            0x63, 0x00, 0x6B, 0x00, 0x65, 0x00, 0x74, 0x00, 0x20, 0x00, 0x62, 0x00, 0x65, 0x00, 0x67, 0x00, 0x69, 0x00, 0x6E, 0x00, 0x3D};
-    private static final byte[] utf16LEMetadataPrefix = {0x3C, 0x00, 0x3F, 0x00, 0x78, 0x00, 0x70, 0x00, 0x61, 0x00,
-            0x63, 0x00, 0x6B, 0x00, 0x65, 0x00, 0x74, 0x00, 0x20, 0x00, 0x62, 0x00, 0x65, 0x00, 0x67, 0x00, 0x69, 0x00, 0x6E, 0x00, 0x3D, 0x00};
-    private static final byte[] utf32BEMetadataPrefix = {0x00, 0x00, 0x00, 0x3C, 0x00, 0x00, 0x00, 0x3F, 0x00, 0x00, 0x00, 0x78,
+    private static final byte[] UTF8_METADATA_PREFIX_SQ = {0x3C, 0x3F, 0x78, 0x70, 0x61, 0x63, 0x6B,
+            0x65, 0x74, 0x20, 0x62, 0x65, 0x67, 0x69, 0x6E, 0x3D, 0x27, -0x11, -0x45, -0x41, 0x27};
+    private static final byte[] UTF8_METADATA_PREFIX_DQ = {0x3C, 0x3F, 0x78, 0x70, 0x61, 0x63, 0x6B,
+            0x65, 0x74, 0x20, 0x62, 0x65, 0x67, 0x69, 0x6E, 0x3D, 0x22, -0x11, -0x45, -0x41, 0x22};
+    private static final byte[] UTF8_METADATA_PREFIX_SQ_EMPTY = {0x3C, 0x3F, 0x78, 0x70, 0x61, 0x63, 0x6B,
+            0x65, 0x74, 0x20, 0x62, 0x65, 0x67, 0x69, 0x6E, 0x3D, 0x27, 0x27};
+    private static final byte[] UTF8_METADATA_PREFIX_DQ_EMPTY = {0x3C, 0x3F, 0x78, 0x70, 0x61, 0x63, 0x6B,
+            0x65, 0x74, 0x20, 0x62, 0x65, 0x67, 0x69, 0x6E, 0x3D, 0x22, 0x22};
+    private static final byte[] UTF16BE_METADATA_PREFIX_SQ = {0x00, 0x3C, 0x00, 0x3F, 0x00, 0x78, 0x00, 0x70, 0x00, 0x61, 0x00,
+            0x63, 0x00, 0x6B, 0x00, 0x65, 0x00, 0x74, 0x00, 0x20, 0x00, 0x62, 0x00, 0x65, 0x00, 0x67, 0x00, 0x69, 0x00, 0x6E, 0x00, 0x3D,
+            0x00, 0x27, -0x02, -0x01, 0x00, 0x27};
+    private static final byte[] UTF16BE_METADATA_PREFIX_DQ = {0x00, 0x3C, 0x00, 0x3F, 0x00, 0x78, 0x00, 0x70, 0x00, 0x61, 0x00,
+            0x63, 0x00, 0x6B, 0x00, 0x65, 0x00, 0x74, 0x00, 0x20, 0x00, 0x62, 0x00, 0x65, 0x00, 0x67, 0x00, 0x69, 0x00, 0x6E, 0x00, 0x3D,
+            0x00, 0x22, -0x02, -0x01, 0x00, 0x22};
+    private static final byte[] UTF16LE_METADATA_PREFIX_SQ = {0x3C, 0x00, 0x3F, 0x00, 0x78, 0x00, 0x70, 0x00, 0x61, 0x00,
+            0x63, 0x00, 0x6B, 0x00, 0x65, 0x00, 0x74, 0x00, 0x20, 0x00, 0x62, 0x00, 0x65, 0x00, 0x67, 0x00, 0x69, 0x00, 0x6E, 0x00, 0x3D, 0x00,
+            0x27, 0x00, -0x01, -0x02, 0x27, 0x00};
+    private static final byte[] UTF16LE_METADATA_PREFIX_DQ = {0x3C, 0x00, 0x3F, 0x00, 0x78, 0x00, 0x70, 0x00, 0x61, 0x00,
+            0x63, 0x00, 0x6B, 0x00, 0x65, 0x00, 0x74, 0x00, 0x20, 0x00, 0x62, 0x00, 0x65, 0x00, 0x67, 0x00, 0x69, 0x00, 0x6E, 0x00, 0x3D, 0x00,
+            0x22, 0x00, -0x01, -0x02, 0x22, 0x00};
+    private static final byte[] UTF32BE_METADATA_PREFIX_SQ = {0x00, 0x00, 0x00, 0x3C, 0x00, 0x00, 0x00, 0x3F, 0x00, 0x00, 0x00, 0x78,
             0x00, 0x00, 0x00, 0x70, 0x00, 0x00, 0x00, 0x61, 0x00, 0x00, 0x00, 0x63, 0x00, 0x00, 0x00, 0x6B,
             0x00, 0x00, 0x00, 0x65, 0x00, 0x00, 0x00, 0x74, 0x00, 0x00, 0x00, 0x20, 0x00, 0x00, 0x00, 0x62,
-            0x00, 0x00, 0x00, 0x65, 0x00, 0x00, 0x00, 0x67, 0x00, 0x00, 0x00, 0x69, 0x00, 0x00, 0x00, 0x6E, 0x00, 0x00, 0x00, 0x3D};
-    private static final byte[] utf32LEMetadataPrefix = {0x3C, 0x00, 0x00, 0x00, 0x3F, 0x00, 0x00, 0x00, 0x78,
+            0x00, 0x00, 0x00, 0x65, 0x00, 0x00, 0x00, 0x67, 0x00, 0x00, 0x00, 0x69, 0x00, 0x00, 0x00, 0x6E, 0x00, 0x00, 0x00, 0x3D,
+            0x00, 0x00, 0x00, 0x27, 0x00, 0x00, -0x02, -0x01, 0x00, 0x00, 0x00, 0x27};
+    private static final byte[] UTF32BE_METADATA_PREFIX_DQ = {0x00, 0x00, 0x00, 0x3C, 0x00, 0x00, 0x00, 0x3F, 0x00, 0x00, 0x00, 0x78,
             0x00, 0x00, 0x00, 0x70, 0x00, 0x00, 0x00, 0x61, 0x00, 0x00, 0x00, 0x63, 0x00, 0x00, 0x00, 0x6B,
             0x00, 0x00, 0x00, 0x65, 0x00, 0x00, 0x00, 0x74, 0x00, 0x00, 0x00, 0x20, 0x00, 0x00, 0x00, 0x62,
-            0x00, 0x00, 0x00, 0x65, 0x00, 0x00, 0x00, 0x67, 0x00, 0x00, 0x00, 0x69, 0x00, 0x00, 0x00, 0x6E, 0x00, 0x00, 0x00, 0x3D, 0x00, 0x00, 0x00};
-    private static final byte[] utf8SingleQuote = {0x27};
-    private static final byte[] utf8DoubleQuote = {0x22};
-    private static final byte[] utf16BESingleQuote = {0x00, 0x27};
-    private static final byte[] utf16BEDoubleQuote = {0x00, 0x22};
-    private static final byte[] utf16LESingleQuote = {0x27, 0x00};
-    private static final byte[] utf16LEDoubleQuote = {0x22, 0x00};
-    private static final byte[] utf32BESingleQuote = {0x00, 0x00, 0x00, 0x27};
-    private static final byte[] utf32BEDoubleQuote = {0x00, 0x00, 0x00, 0x22};
-    private static final byte[] utf32LESingleQuote = {0x27, 0x00, 0x00, 0x00};
-    private static final byte[] utf32LEDoubleQuote = {0x22, 0x00, 0x00, 0x00};
-
-    private static final byte[] utf8Begin = {-0x11, -0x45, -0x41};
-    private static final byte[] utf16BEBegin = {-0x02, -0x01};
-    private static final byte[] utf16LEBegin = {-0x01, -0x02};
-    private static final byte[] utf32BEBegin = {0x00, 0x00, -0x02, -0x01};
-    private static final byte[] utf32LEBegin = {-0x01, -0x02, 0x00, 0x00};
+            0x00, 0x00, 0x00, 0x65, 0x00, 0x00, 0x00, 0x67, 0x00, 0x00, 0x00, 0x69, 0x00, 0x00, 0x00, 0x6E, 0x00, 0x00, 0x00, 0x3D,
+            0x00, 0x00, 0x00, 0x22, 0x00, 0x00, -0x02, -0x01, 0x00, 0x00, 0x00, 0x22};
+    private static final byte[] UTF32LE_METADATA_PREFIX_SQ = {0x3C, 0x00, 0x00, 0x00, 0x3F, 0x00, 0x00, 0x00, 0x78,
+            0x00, 0x00, 0x00, 0x70, 0x00, 0x00, 0x00, 0x61, 0x00, 0x00, 0x00, 0x63, 0x00, 0x00, 0x00, 0x6B,
+            0x00, 0x00, 0x00, 0x65, 0x00, 0x00, 0x00, 0x74, 0x00, 0x00, 0x00, 0x20, 0x00, 0x00, 0x00, 0x62,
+            0x00, 0x00, 0x00, 0x65, 0x00, 0x00, 0x00, 0x67, 0x00, 0x00, 0x00, 0x69, 0x00, 0x00, 0x00, 0x6E, 0x00, 0x00, 0x00, 0x3D, 0x00, 0x00, 0x00,
+            0x27, 0x00, 0x00, 0x00, -0x01, -0x02, 0x00, 0x00, 0x27, 0x00, 0x00, 0x00};
+    private static final byte[] UTF32LE_METADATA_PREFIX_DQ = {0x3C, 0x00, 0x00, 0x00, 0x3F, 0x00, 0x00, 0x00, 0x78,
+            0x00, 0x00, 0x00, 0x70, 0x00, 0x00, 0x00, 0x61, 0x00, 0x00, 0x00, 0x63, 0x00, 0x00, 0x00, 0x6B,
+            0x00, 0x00, 0x00, 0x65, 0x00, 0x00, 0x00, 0x74, 0x00, 0x00, 0x00, 0x20, 0x00, 0x00, 0x00, 0x62,
+            0x00, 0x00, 0x00, 0x65, 0x00, 0x00, 0x00, 0x67, 0x00, 0x00, 0x00, 0x69, 0x00, 0x00, 0x00, 0x6E, 0x00, 0x00, 0x00, 0x3D, 0x00, 0x00, 0x00,
+            0x22, 0x00, 0x00, 0x00, -0x01, -0x02, 0x00, 0x00, 0x22, 0x00, 0x00, 0x00};
 
     private XMLFeaturesReport() {
 
@@ -190,17 +200,22 @@ public final class XMLFeaturesReport {
                 metadata.setAttribute(attr.getKey(), replaceInvalidCharacters(attr.getValue()));
             }
             try {
-                DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-                factory.setNamespaceAware(true);
-                DocumentBuilder builder = factory.newDocumentBuilder();
-                Document metadataDocument = builder.parse(new ByteArrayInputStream((byte[]) metadataNode.getValue()));
-                Node pack = doc.importNode(metadataDocument.getDocumentElement(), true);
-                pack.normalize();
-                metadata.appendChild(pack);
+                InputSource is = getInputSourceWithEncoding((byte[]) metadataNode.getValue());
+                if (is != null) {
+                    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+                    factory.setNamespaceAware(true);
+                    DocumentBuilder builder = factory.newDocumentBuilder();
+                    Document metadataDocument = builder.parse(is);
+                    Node pack = doc.importNode(metadataDocument.getDocumentElement(), true);
+                    pack.normalize();
+                    metadata.appendChild(pack);
+                } else {
+                    LOGGER.debug("Metadata stream does not contains valid prefix.");
+                    parseMetadataError(collection, metadata);
+                }
             } catch (ParserConfigurationException | SAXException | IOException e) {
                 LOGGER.debug("Caught exception and checking XML String.", e);
-                metadata.setAttribute(ErrorsHelper.ERRORID, ErrorsHelper.METADATAPARSER_ID);
-                ErrorsHelper.addErrorIntoCollection(collection, ErrorsHelper.METADATAPARSER_ID, ErrorsHelper.METADATAPARSER_MESSAGE);
+                parseMetadataError(collection, metadata);
             }
 
             return metadata;
@@ -240,86 +255,57 @@ public final class XMLFeaturesReport {
         }
     }
 
-    private static String getEncodingWithBegin(byte[] bStream, int beginOffset) {
-        if (beginOffset >= 0) {
-            byte[] suffix = Arrays.copyOfRange(bStream, beginOffset, bStream.length);
-            if (startsWith(suffix, utf32BEBegin)) {
-                return "UTF-32BE";
-            } else if (startsWith(suffix, utf32LEBegin)) {
-                return "UTF-32LE";
-            } else if (startsWith(suffix, utf8Begin)) {
-                return "UTF-8";
-            } else if (startsWith(suffix, utf16BEBegin)) {
-                return "UTF-16BE";
-            } else if (startsWith(suffix, utf16LEBegin)) {
-                return "UTF-16LE";
-            }
-        }
-        return "UTF-8";
+    private static void parseMetadataError(FeaturesCollection collection, Element metadata) {
+        metadata.setAttribute(ErrorsHelper.ERRORID, ErrorsHelper.METADATAPARSER_ID);
+        ErrorsHelper.addErrorIntoCollection(collection, ErrorsHelper.METADATAPARSER_ID, ErrorsHelper.METADATAPARSER_MESSAGE);
     }
 
-    private static String getStringFromMeta(byte[] stream) {
-        int beginOffset = -1;
-        int packageBeginOffset = 0;
-        if (stream != null) {
-            for (int i = 0; i < stream.length; ++i) {
-                if (stream[i] == 0x3C) {
-                    beginOffset = getBegOffsetFromCutted(Arrays.copyOfRange(stream, i, stream.length));
-                    if (beginOffset >= 0) {
-                        packageBeginOffset = i;
-                        beginOffset += i;
-                        break;
-                    }
+    private static InputSource getInputSourceWithEncoding(byte[] array) {
+        if (array != null) {
+            for (int i = 0; i < array.length; ++i) {
+                String encoding = getEncodingWithBegin(array, i);
+                if (encoding != null) {
+                    ByteArrayInputStream is = new ByteArrayInputStream(Arrays.copyOfRange(array, i, array.length));
+                    InputSource source = new InputSource(is);
+                    source.setEncoding(encoding);
+                    return source;
                 }
             }
-            return new String(Arrays.copyOfRange(stream, packageBeginOffset, stream.length), Charset.forName(getEncodingWithBegin(stream, beginOffset)));
         }
 
         return null;
     }
 
-    private static int getBegOffsetFromCutted(byte[] bStream) {
-        int beginOffset = -1;
-        if ((bStream.length >= utf32LEMetadataPrefix.length + utf32LEDoubleQuote.length + utf32LEBegin.length) &&
-                startsWith(bStream, utf32LEMetadataPrefix)) {
-            byte[] quote = Arrays.copyOfRange(bStream, utf32LEMetadataPrefix.length, utf32LEMetadataPrefix.length + utf32LEDoubleQuote.length);
-            if (startsWith(quote, utf32LESingleQuote) || startsWith(quote, utf32LEDoubleQuote)) {
-                beginOffset = utf32LEMetadataPrefix.length + utf32LEDoubleQuote.length;
-            }
-        } else if ((bStream.length >= utf32BEMetadataPrefix.length + utf32BEDoubleQuote.length + utf32BEBegin.length) &&
-                startsWith(bStream, utf32BEMetadataPrefix)) {
-            byte[] quote = Arrays.copyOfRange(bStream, utf32BEMetadataPrefix.length, utf32BEMetadataPrefix.length + utf32BEDoubleQuote.length);
-            if (startsWith(quote, utf32BESingleQuote) || startsWith(quote, utf32BEDoubleQuote)) {
-                beginOffset = utf32BEMetadataPrefix.length + utf32BEDoubleQuote.length;
-            }
-        } else if ((bStream.length >= utf16LEMetadataPrefix.length + utf16LEDoubleQuote.length + utf16LEBegin.length) &&
-                startsWith(bStream, utf16LEMetadataPrefix)) {
-            byte[] quote = Arrays.copyOfRange(bStream, utf16LEMetadataPrefix.length, utf16LEMetadataPrefix.length + utf16LEDoubleQuote.length);
-            if (startsWith(quote, utf16LESingleQuote) || startsWith(quote, utf16LEDoubleQuote)) {
-                beginOffset = utf16LEMetadataPrefix.length + utf16LEDoubleQuote.length;
-            }
-        } else if ((bStream.length >= utf16BEMetadataPrefix.length + utf16BEDoubleQuote.length + utf16BEBegin.length) &&
-                startsWith(bStream, utf16BEMetadataPrefix)) {
-            byte[] quote = Arrays.copyOfRange(bStream, utf16BEMetadataPrefix.length, utf16BEMetadataPrefix.length + utf16BEDoubleQuote.length);
-            if (startsWith(quote, utf16BESingleQuote) || startsWith(quote, utf16BEDoubleQuote)) {
-                beginOffset = utf16BEMetadataPrefix.length + utf16BEDoubleQuote.length;
-            }
-        } else if ((bStream.length >= utf8MetadataPrefix.length + utf8DoubleQuote.length + utf8Begin.length) &&
-                startsWith(bStream, utf8MetadataPrefix)) {
-            byte[] quote = Arrays.copyOfRange(bStream, utf8MetadataPrefix.length, utf8MetadataPrefix.length + utf8DoubleQuote.length);
-            if (startsWith(quote, utf8SingleQuote) || startsWith(quote, utf8DoubleQuote)) {
-                beginOffset = utf8MetadataPrefix.length + utf8DoubleQuote.length;
+    private static String getEncodingWithBegin(byte[] bStream, int beginOffset) {
+        if (beginOffset >= 0) {
+            if (matchesFrom(bStream, beginOffset, UTF32BE_METADATA_PREFIX_DQ) ||
+                    matchesFrom(bStream, beginOffset, UTF32BE_METADATA_PREFIX_SQ)) {
+                return "UTF-32BE";
+            } else if (matchesFrom(bStream, beginOffset, UTF32LE_METADATA_PREFIX_DQ) ||
+                    matchesFrom(bStream, beginOffset, UTF32LE_METADATA_PREFIX_SQ)) {
+                return "UTF-32LE";
+            } else if (matchesFrom(bStream, beginOffset, UTF16BE_METADATA_PREFIX_DQ) ||
+                    matchesFrom(bStream, beginOffset, UTF16BE_METADATA_PREFIX_SQ)) {
+                return "UTF-16BE";
+            } else if (matchesFrom(bStream, beginOffset, UTF16LE_METADATA_PREFIX_DQ) ||
+                    matchesFrom(bStream, beginOffset, UTF16LE_METADATA_PREFIX_SQ)) {
+                return "UTF-16LE";
+            } else if (matchesFrom(bStream, beginOffset, UTF8_METADATA_PREFIX_DQ) ||
+                    matchesFrom(bStream, beginOffset, UTF8_METADATA_PREFIX_DQ_EMPTY) ||
+                    matchesFrom(bStream, beginOffset, UTF8_METADATA_PREFIX_SQ) ||
+                    matchesFrom(bStream, beginOffset, UTF8_METADATA_PREFIX_SQ_EMPTY)) {
+                return "UTF-8";
             }
         }
-        return beginOffset;
+        return null;
     }
 
-    private static boolean startsWith(byte[] source, byte[] match) {
-        if (match.length > source.length) {
+    private static boolean matchesFrom(byte[] source, int from, byte[] match) {
+        if (match.length > source.length - from) {
             return false;
         }
         for (int i = 0; i < match.length; i++) {
-            if (source[i] != match[i]) {
+            if (source[i + from] != match[i]) {
                 return false;
             }
         }
