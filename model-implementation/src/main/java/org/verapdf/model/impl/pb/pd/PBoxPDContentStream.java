2c69eeff9457d606f7cda113f0fc4916d07403a1
Timur Kamalov <timur.kamalov@duallab.com>
2015-09-28 13:34:26 +0300

    Fixed bug caused by pages without contents.

--- model-implementation/src/main/java/org/verapdf/model/impl/pb/pd/PBoxPDContentStream.java
+++ model-implementation/src/main/java/org/verapdf/model/impl/pb/pd/PBoxPDContentStream.java
@@ -1,6 +1,7 @@
 package org.verapdf.model.impl.pb.pd;
 
 import org.apache.log4j.Logger;
+import org.apache.pdfbox.cos.COSStream;
 import org.apache.pdfbox.pdfparser.PDFStreamParser;
 import org.verapdf.model.baselayer.Object;
 import org.verapdf.model.factory.operator.OperatorFactory;
@@ -8,7 +9,6 @@ import org.verapdf.model.operator.Operator;
 import org.verapdf.model.pdlayer.PDContentStream;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -40,12 +40,15 @@ public class PBoxPDContentStream extends PBoxPDObject implements
 
     private List<Operator> getOperators() {
         try {
-            PDFStreamParser streamParser = new PDFStreamParser(
-                    this.contentStream.getContentStream(), true);
-            streamParser.parse();
-			List<Operator> result = OperatorFactory.operatorsFromTokens(streamParser.getTokens(),
-                    this.contentStream.getResources());
-			return Collections.unmodifiableList(result);
+            COSStream cStream = this.contentStream.getContentStream();
+            if (cStream != null) {
+                PDFStreamParser streamParser = new PDFStreamParser(
+                        this.contentStream.getContentStream(), true);
+                streamParser.parse();
+                List<Operator> result = OperatorFactory.operatorsFromTokens(streamParser.getTokens(),
+                        this.contentStream.getResources());
+                return Collections.unmodifiableList(result);
+            }
         } catch (IOException e) {
             LOGGER.error(
                     "Error while parsing content stream. " + e.getMessage(), e);
