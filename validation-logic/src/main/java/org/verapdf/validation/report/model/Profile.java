319672511cbdc056dd06aba718def396eeac9bf9
Evgeniy Muravitskiy <evgeniy.muravitskiy@duallab.com>
2015-09-30 15:21:20 +0300

    Fixes in validator

--- validation-logic/src/main/java/org/verapdf/validation/report/model/Profile.java
+++ validation-logic/src/main/java/org/verapdf/validation/report/model/Profile.java
@@ -1,13 +1,17 @@
 package org.verapdf.validation.report.model;
 
+import org.verapdf.validation.profile.model.ValidationProfile;
+
 /**
  * Structure of the profile info in validation info.
  *
  * @author Maksim Bezrukov
  */
 public class Profile {
+
 	private final String name;
 	private final String hash;
+	private final ValidationProfile profile;
 
 	/**
 	 * Creates model of an information about used profile
@@ -15,9 +19,10 @@ public class Profile {
 	 * @param name name of the validation profile
 	 * @param hash hash code of the validation profile
 	 */
-	public Profile(final String name, final String hash) {
+	public Profile(final String name, final String hash, ValidationProfile profile) {
 		this.name = name;
 		this.hash = hash;
+		this.profile = profile;
 	}
 
 	/**
@@ -33,4 +38,12 @@ public class Profile {
 	public String getHash() {
 		return hash;
 	}
+
+	/**
+	 * @return representation of validation profile
+	 */
+	public ValidationProfile getProfile() {
+		return profile;
+	}
+
 }
