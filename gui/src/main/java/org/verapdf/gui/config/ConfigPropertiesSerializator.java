1eff56cebbe0695ae56581c1ddcf36a219a59df8
Maksim Bezrukov <maksim.bezrukov@duallab.com>
2015-10-14 13:03:37 +0300

    Removed mutable settings. Changed for immutable Config.

--- gui/src/main/java/org/verapdf/gui/config/ConfigPropertiesSerializator.java
+++ gui/src/main/java/org/verapdf/gui/config/ConfigPropertiesSerializator.java
@@ -16,12 +16,10 @@ public final class ConfigPropertiesSerializator {
 
 	private static final Logger LOGGER = Logger.getLogger(ConfigPropertiesSerializator.class);
 
-	private static final String PROPERTY_PROCESSING_TYPE = "processingType";
 	private static final String PROPERTY_SHOW_PASSED_RULES = "showPassedRules";
 	private static final String PROPERTY_MAX_NUMBER_FAILED_CHECKS = "maxNumbFailedChecks";
 	private static final String PROPERTY_MAX_NUMBER_DISPLAYED_FAILED_CHECKS = "maxNumbDisplFailedChecks";
 	private static final String PROPERTY_FEATURES_CONFIG_FILE = "featuresPluginConfigFile";
-	private static final String PROPERTY_FIX_METADATA = "fixMetadata";
 	private static final String PROPERTY_METADATA_FIXER_PREFIX = "metadataFixerPrefix";
 	private static final String PROPERTY_FIX_METADATA_PATH_FOLDER = "fixMetadataPathFolder";
 
@@ -46,12 +44,10 @@ public final class ConfigPropertiesSerializator {
 		FileWriter writer = new FileWriter(path.toFile());
 
 		Properties settings = new Properties();
-		settings.setProperty(PROPERTY_PROCESSING_TYPE, String.valueOf(config.getProcessingType()));
 		settings.setProperty(PROPERTY_MAX_NUMBER_FAILED_CHECKS, String.valueOf(config.getMaxNumberOfFailedChecks()));
 		settings.setProperty(PROPERTY_MAX_NUMBER_DISPLAYED_FAILED_CHECKS, String.valueOf(config.getMaxNumberOfDisplayedFailedChecks()));
 		settings.setProperty(PROPERTY_SHOW_PASSED_RULES, String.valueOf(config.isShowPassedRules()));
 		settings.setProperty(PROPERTY_FEATURES_CONFIG_FILE, config.getFeaturesPluginsConfigFilePath().toString());
-		settings.setProperty(PROPERTY_FIX_METADATA, String.valueOf(config.isFixMetadata()));
 		settings.setProperty(PROPERTY_METADATA_FIXER_PREFIX, config.getMetadataFixerPrefix());
 		settings.setProperty(PROPERTY_FIX_METADATA_PATH_FOLDER, config.getFixMetadataPathFolder().toString());
 		settings.store(writer, "settings");
@@ -122,11 +118,6 @@ public final class ConfigPropertiesSerializator {
 		Config.Builder builder = new Config.Builder();
 
 		try {
-			builder.processingType(getIntegerValue(properties, PROPERTY_PROCESSING_TYPE));
-		} catch (IllegalArgumentException e) {
-			LOGGER.error("Property " + PROPERTY_PROCESSING_TYPE + " is missing or containing a wrong value. Setting it to default");
-		}
-		try {
 			builder.maxNumberOfFailedChecks(getIntegerValue(properties, PROPERTY_MAX_NUMBER_FAILED_CHECKS));
 		} catch (IllegalArgumentException e) {
 			LOGGER.error("Property " + PROPERTY_MAX_NUMBER_FAILED_CHECKS + " is missing or containing a wrong value. Setting it to default");
@@ -140,11 +131,6 @@ public final class ConfigPropertiesSerializator {
 			builder.showPassedRules(getBooleanValue(properties, PROPERTY_SHOW_PASSED_RULES));
 		} catch (IllegalArgumentException e) {
 			LOGGER.error("Property " + PROPERTY_SHOW_PASSED_RULES + " is missing or containing a wrong value. Setting it to default");
-		}
-		try {
-			builder.fixMetadata(getBooleanValue(properties, PROPERTY_FIX_METADATA));
-		} catch (IllegalArgumentException e) {
-			LOGGER.error("Property " + PROPERTY_FIX_METADATA + " is missing or containing a wrong value. Setting it to default");
 		}
 		try {
 			builder.metadataFixerPrefix(getStringValue(properties, PROPERTY_METADATA_FIXER_PREFIX));
