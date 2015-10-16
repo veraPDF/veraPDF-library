49c8d1a13e303240750b0a2fc0c1cecb781cc804
Evgeniy Muravitskiy <evgeniy.muravitskiy@duallab.com>
2015-10-16 15:19:40 +0300

    Fixes in id generation for glyphs

--- model-implementation/src/main/java/org/verapdf/model/impl/pb/operator/textshow/PBGlyph.java
+++ model-implementation/src/main/java/org/verapdf/model/impl/pb/operator/textshow/PBGlyph.java
@@ -1,5 +1,6 @@
 package org.verapdf.model.impl.pb.operator.textshow;
 
+import org.apache.pdfbox.pdmodel.font.PDFont;
 import org.verapdf.model.GenericModelObject;
 import org.verapdf.model.operator.Glyph;
 import org.verapdf.model.tools.IDGenerator;
@@ -11,29 +12,21 @@ public class PBGlyph extends GenericModelObject implements Glyph {
 
 	public final static String GLYPH_TYPE = "Glyph";
 
+	private final String id;
+
 	private Boolean glyphPresent;
 	private Boolean widthsConsistent;
 
-	//used for id
-	private String fontName;
-	private int glyphCode;
-
-	public PBGlyph(Boolean glyphPresent, Boolean widthsConsistent, String fontName, int glyphCode) {
-		super(GLYPH_TYPE);
-		this.glyphPresent = glyphPresent;
-		this.widthsConsistent = widthsConsistent;
-
-		this.fontName = fontName;
-		this.glyphCode = glyphCode;
+	public PBGlyph(Boolean glyphPresent, Boolean widthsConsistent, PDFont font, int glyphCode) {
+		this(glyphPresent, widthsConsistent, font, glyphCode, GLYPH_TYPE);
 	}
 
-	public PBGlyph(Boolean glyphPresent, Boolean widthsConsistent, String fontName, int glyphCode, String type) {
+	public PBGlyph(Boolean glyphPresent, Boolean widthsConsistent, PDFont font, int glyphCode, String type) {
 		super(type);
 		this.glyphPresent = glyphPresent;
 		this.widthsConsistent = widthsConsistent;
 
-		this.fontName = fontName;
-		this.glyphCode = glyphCode;
+		this.id = IDGenerator.generateID(font.getCOSObject().hashCode(), font.getName(), glyphCode);
 	}
 
 	@Override
@@ -48,7 +41,7 @@ public class PBGlyph extends GenericModelObject implements Glyph {
 
 	@Override
 	public String getID() {
-		return IDGenerator.generateID(this.fontName, this.glyphCode);
+		return id;
 	}
 
 }
