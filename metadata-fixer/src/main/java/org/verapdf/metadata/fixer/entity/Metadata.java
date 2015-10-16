41ca548cf223d2bdcaa3488ecb2aebe287b9b6c7
Evgeniy Muravitskiy <evgeniy.muravitskiy@duallab.com>
2015-10-16 14:31:51 +0300

    Fixes in metadata interface

--- metadata-fixer/src/main/java/org/verapdf/metadata/fixer/entity/Metadata.java
+++ metadata-fixer/src/main/java/org/verapdf/metadata/fixer/entity/Metadata.java
@@ -4,6 +4,7 @@ import org.verapdf.metadata.fixer.MetadataFixerResult;
 import org.verapdf.metadata.fixer.schemas.AdobePDF;
 import org.verapdf.metadata.fixer.schemas.DublinCore;
 import org.verapdf.metadata.fixer.schemas.XMPBasic;
+import org.verapdf.metadata.fixer.utils.PDFAFlavour;
 
 /**
  * @author Evgeniy Muravitskiy
@@ -12,7 +13,7 @@ public interface Metadata {
 
 	void removePDFIdentificationSchema(MetadataFixerResult result);
 
-	void addPDFIdentificationSchema(MetadataFixerResult report);
+	void addPDFIdentificationSchema(MetadataFixerResult report, PDFAFlavour flavour);
 
 	DublinCore getDublinCoreSchema(InfoDictionary info);
 
