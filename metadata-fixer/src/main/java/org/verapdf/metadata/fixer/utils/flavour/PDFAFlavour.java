135be9d7e59c5cbe746d69f6234f40c546c1217b
Evgeniy Muravitskiy <evgeniy.muravitskiy@duallab.com>
2015-10-19 20:00:49 +0300

    Move Part, Level and Iso to Flavour

--- metadata-fixer/src/main/java/org/verapdf/metadata/fixer/utils/flavour/PDFAFlavour.java
+++ metadata-fixer/src/main/java/org/verapdf/metadata/fixer/utils/flavour/PDFAFlavour.java
@@ -1,19 +1,20 @@
 package org.verapdf.metadata.fixer.utils.flavour;
 
+import java.awt.*;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-import static org.verapdf.metadata.fixer.utils.flavour.Part.NO_STANDARD;
-import static org.verapdf.metadata.fixer.utils.flavour.Part.ISO_19005_1;
-import static org.verapdf.metadata.fixer.utils.flavour.Part.ISO_19005_2;
-import static org.verapdf.metadata.fixer.utils.flavour.Part.ISO_19005_3;
+import static org.verapdf.metadata.fixer.utils.flavour.PDFAFlavour.Part.NO_STANDARD;
+import static org.verapdf.metadata.fixer.utils.flavour.PDFAFlavour.Part.ISO_19005_1;
+import static org.verapdf.metadata.fixer.utils.flavour.PDFAFlavour.Part.ISO_19005_2;
+import static org.verapdf.metadata.fixer.utils.flavour.PDFAFlavour.Part.ISO_19005_3;
 
-import static org.verapdf.metadata.fixer.utils.flavour.Level.NO_LEVEL;
-import static org.verapdf.metadata.fixer.utils.flavour.Level.A;
-import static org.verapdf.metadata.fixer.utils.flavour.Level.B;
-import static org.verapdf.metadata.fixer.utils.flavour.Level.U;
+import static org.verapdf.metadata.fixer.utils.flavour.PDFAFlavour.Level.NO_LEVEL;
+import static org.verapdf.metadata.fixer.utils.flavour.PDFAFlavour.Level.A;
+import static org.verapdf.metadata.fixer.utils.flavour.PDFAFlavour.Level.B;
+import static org.verapdf.metadata.fixer.utils.flavour.PDFAFlavour.Level.U;
 
 /**
  * @author Evgeniy Muravitskiy
@@ -39,6 +40,30 @@ public enum PDFAFlavour {
 
 	private static final Map<String, PDFAFlavour> FLAVOURS;
 
+	public static Set<String> getFlavourIds() {
+		return FLAVOURS.keySet();
+	}
+
+	public static PDFAFlavour byFlavourId(String flavourId) {
+		// TODO : do we need to handle case when flavourId is incorrect?
+		return flavourId == null ? null : FLAVOURS.get(flavourId.toUpperCase());
+	}
+
+	static {
+		Map<String, PDFAFlavour> buffer = new HashMap<>(8, 1);
+
+		buffer.put(NO_FLAVOUR.getId(), NO_FLAVOUR);
+		buffer.put(PDFA_1_A.getId()  , PDFA_1_A);
+		buffer.put(PDFA_1_B.getId()  , PDFA_1_B);
+		buffer.put(PDFA_2_A.getId()  , PDFA_2_A);
+		buffer.put(PDFA_2_B.getId()  , PDFA_2_B);
+		buffer.put(PDFA_3_A.getId()  , PDFA_3_A);
+		buffer.put(PDFA_3_B.getId()  , PDFA_3_B);
+		buffer.put(PDFA_3_U.getId()  , PDFA_3_U);
+
+		FLAVOURS = Collections.unmodifiableMap(buffer);
+	}
+
 	private final Part part;
 	private final Level level;
 
@@ -59,28 +84,97 @@ public enum PDFAFlavour {
 		return this.part.getPartNumber() + this.level.getCode();
 	}
 
-	public static PDFAFlavour byFlavourId(String flavourId) {
-		// TODO : do we need to handle case when flavourId is incorrect?
-		return flavourId == null ? null : FLAVOURS.get(flavourId);
+	public enum Part {
+
+		NO_STANDARD(0),
+
+		ISO_19005_1(1),
+
+		ISO_19005_2(2),
+
+		ISO_19005_3(3);
+
+		private final int value;
+
+		Part(int value) {
+			this.value = value;
+		}
+
+		public final int getPartNumber() {
+			return this.value;
+		}
+
+		// TODO : implement me
+		public final String getId() {
+			throw new IllegalComponentStateException("Implement this method");
+		}
+
+		// TODO : implement me
+		public final String getName() {
+			throw new IllegalComponentStateException("Implement this method");
+		}
+
+		// TODO : implement me
+		public final String getDescription() {
+			throw new IllegalComponentStateException("Implement this method");
+		}
+
+		// TODO : implement me
+		public final String getYear() {
+			throw new IllegalComponentStateException("Implement this method");
+		}
+
 	}
 
-	public static Set<String> getFlavourIds() {
-		return FLAVOURS.keySet();
+	public enum Level {
+
+		NO_LEVEL('F'),
+
+		A('A'),
+
+		B('B'),
+
+		U('U');
+
+		private final char value;
+
+		Level(char value) {
+			this.value = value;
+		}
+
+		// TODO : implement me
+		public String getName() {
+			throw new IllegalComponentStateException("Implement this method");
+		}
+
+		public String getCode() {
+			return String.valueOf(this.value);
+		}
+
 	}
 
-	static {
-		Map<String, PDFAFlavour> buffer = new HashMap<>(8, 1);
+	public enum IsoStandardSeries {
 
-		buffer.put(NO_FLAVOUR.getId(), NO_FLAVOUR);
-		buffer.put(PDFA_1_A.getId()  , PDFA_1_A);
-		buffer.put(PDFA_1_B.getId()  , PDFA_1_B);
-		buffer.put(PDFA_2_A.getId()  , PDFA_2_A);
-		buffer.put(PDFA_2_B.getId()  , PDFA_2_B);
-		buffer.put(PDFA_3_A.getId()  , PDFA_3_A);
-		buffer.put(PDFA_3_B.getId()  , PDFA_3_B);
-		buffer.put(PDFA_3_U.getId()  , PDFA_3_U);
+		ISO_19005,
 
-		FLAVOURS = Collections.unmodifiableMap(buffer);
+		ISO_32000,
+
+		NO_SERIES;
+
+		// TODO : implement me
+		public final String getId() {
+			throw new IllegalComponentStateException("Implement this method");
+		}
+
+		// TODO : implement me
+		public final String getName() {
+			throw new IllegalComponentStateException("Implement this method");
+		}
+
+		// TODO : implement me
+		public final String getDescription() {
+			throw new IllegalComponentStateException("Implement this method");
+		}
 	}
 
 }
