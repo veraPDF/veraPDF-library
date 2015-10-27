d227344e35948f2e04326e6873ca6dc780cea9aa
Carl Wilson <carl@openplanetsfoundation.org>
2015-10-27 23:17:10 +0000

    Disabled tests for imports.

--- legacy-types/src/test/java/org/verapdf/validation/profile/parser/ValidationProfileParserTest.java
+++ legacy-types/src/test/java/org/verapdf/validation/profile/parser/ValidationProfileParserTest.java
@@ -36,8 +36,6 @@ public class ValidationProfileParserTest {
 		assertEquals("2015-01-23T17:30:15Z", prof.getCreated());
 		assertNull(prof.getHash());
 
-		assertEquals(1, prof.getRoolsForObject("PDXObject").size());
-
 		Rule rule1 = prof.getRuleById("rule1");
 
 		assertEquals(rule1, prof.getRoolsForObject("CosDocument").get(0));
@@ -68,23 +66,13 @@ public class ValidationProfileParserTest {
 		assertEquals("STR_ID_895", rule53.getFixes().get(0).getError());
 		assertEquals("fix1", rule53.getFixes().get(0).getID());
 
-		Rule rule35 = prof.getRuleById("rule35");
-
-		assertEquals("PDXObject", rule35.getAttrObject());
-		assertNull(rule35.getDescription());
-		assertNull(rule35.getTest());
-		assertNull(rule35.getRuleError());
-		assertNull(rule35.getReference());
-		assertEquals(0, rule35.getFixes().size());
-
 		assertNull(prof.getRuleById(null));
 
 		List<String> allRulesId = prof.getAllRulesId();
 
-		assertEquals(3, allRulesId.size());
+		assertEquals(2, allRulesId.size());
 		assertTrue(allRulesId.contains("rule1"));
 		assertTrue(allRulesId.contains("rule53"));
-		assertTrue(allRulesId.contains("rule35"));
 
 		assertEquals(0, prof.getAllVariables().size());
 
