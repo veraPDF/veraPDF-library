1b0b06a959e410c110f52bb7c86f48311743a9cc
Evgeniy Muravitskiy <evgeniy.muravitskiy@duallab.com>
2015-11-03 15:34:15 +0200

    Add new type of resources

---
+++ model-implementation/src/main/java/org/verapdf/model/tools/PDExtendedResources.java
@@ -0,0 +1,129 @@
+package org.verapdf.model.tools;
+
+import org.apache.log4j.Logger;
+import org.apache.pdfbox.cos.COSDictionary;
+import org.apache.pdfbox.cos.COSName;
+import org.apache.pdfbox.pdmodel.PDResources;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.graphics.PDXObject;
+import org.apache.pdfbox.pdmodel.graphics.color.PDColorSpace;
+import org.apache.pdfbox.pdmodel.graphics.pattern.PDAbstractPattern;
+import org.apache.pdfbox.pdmodel.graphics.shading.PDShading;
+import org.apache.pdfbox.pdmodel.graphics.state.PDExtendedGraphicsState;
+
+import java.io.IOException;
+
+/**
+ * @author Evgeniy Muravitskiy
+ */
+public class PDExtendedResources extends PDResources {
+
+	private static final Logger LOGGER = Logger.getLogger(PDExtendedResources.class);
+
+	private final PDResources currentResources;
+	private final PDResources pageResources;
+
+	private PDExtendedResources(PDResources pageResources, COSDictionary currentResources) {
+		super(currentResources);
+		this.pageResources = pageResources;
+		this.currentResources = new PDResources(currentResources);
+	}
+
+	public PDResources getPageResources() {
+		return this.pageResources;
+	}
+
+	@Override
+	public PDFont getFont(COSName name) throws IOException {
+		try {
+			PDFont font = this.currentResources.getFont(name);
+			if (font != null) {
+				return font;
+			}
+		} catch (IOException e) {
+			LOGGER.warn("Problems during font obtain from current resource dictionary. " +
+					"Trying to find it in page dictionary");
+		}
+		return this.pageResources.getFont(name);
+	}
+
+	@Override
+	public PDColorSpace getColorSpace(COSName name) throws IOException {
+		try {
+			PDColorSpace colorSpace = this.currentResources.getColorSpace(name);
+			if (colorSpace != null) {
+				return colorSpace;
+			}
+		} catch (IOException e) {
+			LOGGER.warn("Problems during color space obtain from current resource dictionary. " +
+					"Trying to find it in page dictionary");
+		}
+		return this.pageResources.getColorSpace(name);
+	}
+
+	@Override
+	public PDExtendedGraphicsState getExtGState(COSName name) {
+		PDExtendedGraphicsState state = this.currentResources.getExtGState(name);
+		return state != null ? state : this.pageResources.getExtGState(name);
+	}
+
+	@Override
+	public PDShading getShading(COSName name) throws IOException {
+		try {
+			PDShading shading = this.currentResources.getShading(name);
+			if (shading != null) {
+				return shading;
+			}
+		} catch (IOException e) {
+			LOGGER.warn("Problems during shading obtain from current resource dictionary. " +
+					"Trying to find it in page dictionary");
+		}
+		return this.pageResources.getShading(name);
+	}
+
+	@Override
+	public PDAbstractPattern getPattern(COSName name) throws IOException {
+		try {
+			PDAbstractPattern pattern = this.currentResources.getPattern(name);
+			if (pattern != null) {
+				return pattern;
+			}
+		} catch (IOException e) {
+			LOGGER.warn("Problems during pattern obtain from current resource dictionary. " +
+					"Trying to find it in page dictionary");
+		}
+		return this.pageResources.getPattern(name);
+	}
+
+	@Override
+	public PDXObject getXObject(COSName name) throws IOException {
+		try {
+			PDXObject object = this.currentResources.getXObject(name);
+			if (object != null) {
+				return object;
+			}
+		} catch (IOException e) {
+			LOGGER.warn("Problems during XObject obtain from current resource dictionary. " +
+					"Trying to find it in page dictionary");
+		}
+		return this.pageResources.getXObject(name);
+	}
+
+	@Override
+	public boolean hasColorSpace(COSName name) {
+		return this.currentResources.hasColorSpace(name) || this.pageResources.hasColorSpace(name);
+	}
+
+	public static PDExtendedResources getInstance(PDResources pageResources) {
+		return getInstance(pageResources, null);
+	}
+
+	public static PDExtendedResources getInstance(
+			PDResources pageResources, PDResources currentResources) {
+		COSDictionary current = currentResources == null ? new COSDictionary() :
+														currentResources.getCOSObject();
+		return new PDExtendedResources(pageResources != null ?
+				pageResources : new PDResources(), current);
+	}
+
+}
