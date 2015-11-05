c4b2bbd0dcedc05fc3b9a9daffd432076228c9bf
Evgeniy Muravitskiy <evgeniy.muravitskiy@duallab.com>
2015-11-05 12:14:09 +0200

    Fixes in config

--- metadata-fixer/src/main/java/org/verapdf/metadata/fixer/utils/FixerConfig.java
+++ metadata-fixer/src/main/java/org/verapdf/metadata/fixer/utils/FixerConfig.java
@@ -1,9 +1,7 @@
 package org.verapdf.metadata.fixer.utils;
 
-import org.verapdf.metadata.fixer.entity.Metadata;
 import org.verapdf.metadata.fixer.entity.PDFDocument;
 import org.verapdf.metadata.fixer.utils.parser.ProcessedObjectsParser;
-import org.verapdf.pdfa.flavours.PDFAFlavour;
 import org.verapdf.pdfa.results.ValidationResult;
 
 /**
@@ -13,13 +11,10 @@ public interface FixerConfig {
 
     ValidationResult getValidationResult();
 
-	Metadata getMetadata();
-
 	PDFDocument getDocument();
 
 	ProcessedObjectsParser getParser();
 
 	boolean isFixIdentification();
 
-	PDFAFlavour getPDFAFlavour();
 }
