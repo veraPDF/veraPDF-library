78119a2e1509e42c933b7e4b86d9a40aa7df25c5
Carl Wilson <carl@openplanetsfoundation.org>
2015-11-12 17:23:22 +0000

    Extended testing to four profiles.

--- verapdf-qa/src/test/java/org/verapdf/integration/ITVeraPDFTestSuite.java
+++ verapdf-qa/src/test/java/org/verapdf/integration/ITVeraPDFTestSuite.java
@@ -6,13 +6,21 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Enumeration;
+import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipException;
 import java.util.zip.ZipFile;
+
+import javax.xml.bind.JAXBException;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.verapdf.core.ProfileException;
+import org.verapdf.core.ValidationException;
 import org.verapdf.model.ModelParser;
 import org.verapdf.pdfa.PDFAValidator;
 import org.verapdf.pdfa.flavours.PDFAFlavour;
@@ -22,10 +30,13 @@ import org.verapdf.pdfa.qa.GitHubBackedProfileDirectory;
 import org.verapdf.pdfa.results.ValidationResult;
 import org.verapdf.pdfa.results.ValidationResults;
 import org.verapdf.pdfa.validation.ProfileDirectory;
+import org.verapdf.pdfa.validation.ValidationProfile;
 import org.verapdf.pdfa.validators.Validators;
 
-@SuppressWarnings({"javadoc", "static-method"})
+@SuppressWarnings({ "javadoc", "static-method" })
 public class ITVeraPDFTestSuite {
+    private final static String ZIP_SEPARATOR = "/";
+    private final static String PDF_SUFFIX = ".pdf";
     private static final ProfileDirectory PROFILES = GitHubBackedProfileDirectory.INTEGRATION;
     private static File VERA_CORPUS_ZIP_FILE;
 
@@ -50,32 +61,91 @@ public class ITVeraPDFTestSuite {
 
     @Test
     public void performPdf1BValidation() throws IOException {
+        PDFAFlavour[] filters = new PDFAFlavour[] { PDFAFlavour.PDFA_1_B };
+        validateFlavour(PDFAFlavour.PDFA_1_B, Arrays.asList(filters));
+    }
+
+    @Test
+    public void performPdf1AValidation() throws IOException {
+        PDFAFlavour[] filters = new PDFAFlavour[] { PDFAFlavour.PDFA_1_B,
+                PDFAFlavour.PDFA_1_A };
+        validateFlavour(PDFAFlavour.PDFA_1_A, Arrays.asList(filters));
+    }
+
+    @Test
+    public void performPdf2BValidation() throws IOException {
+        PDFAFlavour[] filters = new PDFAFlavour[] { PDFAFlavour.PDFA_2_B };
+        validateFlavour(PDFAFlavour.PDFA_2_B, Arrays.asList(filters));
+    }
+
+    @Test
+    public void performPdf3BValidation() throws IOException {
+        PDFAFlavour[] filters = new PDFAFlavour[] { PDFAFlavour.PDFA_3_B };
+        validateFlavour(PDFAFlavour.PDFA_3_B, Arrays.asList(filters));
+    }
+
+    private static void validateFlavour(final PDFAFlavour flavour,
+            final List<PDFAFlavour> filters) throws ZipException, IOException {
+
         try (ZipFile zipIn = new ZipFile(VERA_CORPUS_ZIP_FILE);) {
+            ValidationProfile profile = PROFILES
+                    .getValidationProfileByFlavour(flavour);
             Enumeration<? extends ZipEntry> entries = zipIn.entries();
             while (entries.hasMoreElements()) {
                 ZipEntry entry = entries.nextElement();
-                String[] pathParts = entry.getName().split("/");
-                if (entry.isDirectory() || PDFAFlavour.fromString(entry.getName()) != PDFAFlavour.PDFA_1_B)
+                String[] pathParts = entry.getName().split(ZIP_SEPARATOR);
+                if (entry.isDirectory()
+                        || !entry.getName().endsWith(PDF_SUFFIX)
+                        || !matchesFlavourFilter(entry.getName(), filters)) {
                     continue;
-                if (entry.getName().endsWith(".pdf")) {
-                    CorpusItemId corpusId = CorpusItemIdImpl.fromFileName(PDFAFlavour.PDFA_1_B.getPart(), pathParts[pathParts.length - 1]);
+                }
+                CorpusItemId corpusId = CorpusItemIdImpl.fromFileName(
+                        flavour.getPart(), pathParts[pathParts.length - 1]);
+                System.out.println("Testing: " + entry.getName());
+                try (ModelParser loader = new ModelParser(
+                        zipIn.getInputStream(entry))) {
+                    PDFAValidator validator = Validators.validate(profile,
+                            false);
+                    ValidationResult result = validator.validate(loader);
+                    ValidationResults.toXml(result, System.out, Boolean.TRUE);
+                    if (result.isCompliant() != corpusId.getExpectedResult())
+                        System.err.println("Unexpected result:"
+                                + result.isCompliant() + ", for:"
+                                + entry.getName());
+                    // assertTrue("Unexpected result:" + result.isCompliant() +
+                    // ", for:" + entry.getName(), corpusId.getExpectedResult()
+                    // == result.isCompliant());
                     System.out.println();
-                    System.out.println("Testing: " + entry.getName());
-                    try (ModelParser loader = new ModelParser(
-                            zipIn.getInputStream(entry))) {
-                        PDFAValidator validator = Validators.validate(PROFILES.getValidationProfileByFlavour(PDFAFlavour.PDFA_1_B),
-                                false);
-                        ValidationResult result = validator.validate(loader);
-                        ValidationResults.toXml(result, System.out, Boolean.TRUE);
-                        if (result.isCompliant() != corpusId.getExpectedResult())
-                            System.err.println("Unexpected result:" + result.isCompliant() + ", for:" + entry.getName());
-//                        assertTrue("Unexpected result:" + result.isCompliant() + ", for:" + entry.getName(), corpusId.getExpectedResult() == result.isCompliant());
-                        System.out.println();
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
+                } catch (IOException e) {
+                    System.err.println("Error parsing PDF Model for "
+                            + entry.getName());
+                    e.printStackTrace();
+                } catch (ValidationException | NegativeArraySizeException e) {
+                    System.err.println("Error carrying out validation for "
+                            + entry.getName());
+                    e.printStackTrace();
+                    System.err.println(e.getMessage());
+                } catch (JAXBException e) {
+                    System.err
+                            .println("Error marshalling validation result for "
+                                    + entry.getName());
+                    System.err.println(e.getMessage());
                 }
             }
+        } catch (NoSuchElementException excep) {
+            System.err.println("No validation profile found for flavour: "
+                    + flavour);
+            System.err.println(excep.getMessage());
         }
+    }
+
+    private static boolean matchesFlavourFilter(final String parseForMatches,
+            final List<PDFAFlavour> filters) {
+        for (PDFAFlavour flavour : filters) {
+            if (PDFAFlavour.fromString(parseForMatches) == flavour) {
+                return true;
+            }
+        }
+        return false;
     }
 }
