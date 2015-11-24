a1e57cfb98ae23a65e9261a0b914e55c61848f63
Timur Kamalov <timur.kamalov@duallab.com>
2015-11-24 12:33:22 +0300

    Fixed sonar issues : log exception, made ProfileDirectory field transient.

--- verapdf-qa/src/main/java/org/verapdf/pdfa/qa/GitHubBackedProfileDirectory.java
+++ verapdf-qa/src/main/java/org/verapdf/pdfa/qa/GitHubBackedProfileDirectory.java
@@ -3,11 +3,7 @@
  */
 package org.verapdf.pdfa.qa;
 
-import java.io.IOException;
-import java.net.URL;
-import java.util.HashSet;
-import java.util.Set;
-
+import org.apache.log4j.Logger;
 import org.verapdf.core.ProfileException;
 import org.verapdf.pdfa.flavours.PDFAFlavour;
 import org.verapdf.pdfa.validation.ProfileDirectory;
@@ -15,15 +11,24 @@ import org.verapdf.pdfa.validation.Profiles;
 import org.verapdf.pdfa.validation.ValidationProfile;
 import org.verapdf.validation.profile.parser.LegacyProfileConverter;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author <a href="mailto:carl@openpreservation.org">Carl Wilson</a>
  *
  */
 public enum GitHubBackedProfileDirectory implements ProfileDirectory {
+
     /**
      * Integration branch profiles
      */
     INTEGRATION("integration");
+
+    private static final Logger LOGGER = Logger.getLogger(GitHubBackedProfileDirectory.class);
+
     /**
      * This would work for Master branch profiles
      * 
@@ -34,7 +39,7 @@ public enum GitHubBackedProfileDirectory implements ProfileDirectory {
     private static final String PROFILE_PREFIX = "PDFA-";
     private static final String XML_SUFFIX = ".xml";
     private final String branchName;
-    private final ProfileDirectory profiles;
+    private transient final ProfileDirectory profiles;
 
     GitHubBackedProfileDirectory(final String branchName) {
         this.branchName = branchName;
@@ -104,9 +109,8 @@ public enum GitHubBackedProfileDirectory implements ProfileDirectory {
                         .fromLegacyStream(profileURL.openStream(), flavour);
                 profileSet.add(profile);
             } catch (ProfileException | IOException e) {
-                System.err.println("Could not load GitHub profile for "
-                        + flavour.getLevel().getName());
-                System.err.println(e.getMessage());
+                LOGGER.error("Could not load GitHub profile for " + flavour.getLevel().getName());
+                LOGGER.error(e);
             }
         }
         return profileSet;
