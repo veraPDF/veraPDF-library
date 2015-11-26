b3f1750739a1780513168928f9dbc3b6b77085db
Carl Wilson <carl@openplanetsfoundation.org>
2015-11-26 15:00:23 +0000

    RuleSummary now responsible for summarising.

--- gui/src/main/java/org/verapdf/report/RuleSummary.java
+++ gui/src/main/java/org/verapdf/report/RuleSummary.java
@@ -1,83 +1,126 @@
 package org.verapdf.report;
 
-import org.verapdf.pdfa.results.TestAssertion;
-import org.verapdf.pdfa.validation.RuleId;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 
-import java.util.HashSet;
-import java.util.Set;
+import org.verapdf.pdfa.results.TestAssertion;
+import org.verapdf.pdfa.results.TestAssertion.Status;
+import org.verapdf.pdfa.validation.Profiles;
+import org.verapdf.pdfa.validation.RuleId;
 
 /**
  * @author Maksim Bezrukov
  */
 @XmlRootElement
 public class RuleSummary {
-
+    private final Status ruleStatus;
     @XmlAttribute
     private final String specification;
     @XmlAttribute
     private final String clause;
     @XmlAttribute
-    private final String testNumber;
+    private final int testNumber;
     @XmlAttribute
-    private TestAssertion.Status status;
-    @XmlAttribute(name = "checks")
-    private final int totalChecks;
+    private String status;
+    @XmlAttribute
+    private final int passedChecks;
+    @XmlAttribute
+    private final int failedChecks;
     @XmlElement
     private final String description;
-    @XmlElementWrapper
     @XmlElement(name = "check")
     private final Set<Check> checks;
 
-    private RuleSummary(final String specification, final String clause,
-            final String testNumber, final TestAssertion.Status status,
-            final int totalChecks, final String description,
-            final Set<Check> checks) {
-        this.specification = specification;
-        this.clause = clause;
-        this.testNumber = testNumber;
-        this.status = status;
-        this.totalChecks = totalChecks;
-        this.description = description;
-        this.checks = new HashSet<>(checks);
+    private RuleSummary(final RuleId ruleId, final Status status,
+            final int passedChecks, final int failedChecks,
+            final String description, final Set<Check> checks) {
+        this.specification = ruleId.getSpecification().getId();
+        this.clause = ruleId.getClause();
+        this.testNumber = ruleId.getTestNumber();
+        this.ruleStatus = status;
+        this.status = status.toString();
+        this.passedChecks = passedChecks;
+        this.failedChecks = failedChecks;
+        this.description = (status != Status.PASSED) ? description : null;
+        this.checks = ((checks != null) && !checks.isEmpty()) ? new HashSet<>(checks) : null;
+    }
+
+    private RuleSummary(final RuleId ruleId, final Status status,
+            final String description) {
+        this(ruleId, status, 0, 0, description, Collections.<Check> emptySet());
     }
 
     private RuleSummary() {
-        this("", "", "", TestAssertion.Status.PASSED, 0, "",
-                new HashSet<Check>());
+        this(Profiles.defaultRuleId(), Status.UNKNOWN, "description");
+    }
+    
+    /**
+     * @return the {@link Status} of the rule
+     */
+    public Status getRuleStatus() {
+        return this.ruleStatus;
+    }
+    
+    /**
+     * @return the number of passed checks for the rule
+     */
+    public int getPassedChecks() {
+        return this.passedChecks;
     }
 
-    static RuleSummary fromValues(RuleId id, String description,
-            TestAssertion.Status status, int totalChecks) {
+    /**
+     * @return the number of failed checks for the rule
+     */
+    public int getFailedChecks() {
+        return this.failedChecks;
+    }
+
+    static RuleSummary fromValues(final RuleId id, final String description,
+            Set<TestAssertion> assertions, boolean logPassedChecks) {
         if (id == null) {
-            throw new IllegalArgumentException("Argument id con not be null");
-        }
-        if (status == null) {
-            throw new IllegalArgumentException(
-                    "Argument status con not be null");
+            throw new NullPointerException("Argument id can not be null");
         }
         if (description == null) {
-            throw new IllegalArgumentException(
-                    "Argument description con not be null");
+            throw new NullPointerException(
+                    "Argument description can not be null");
         }
-
-        return new RuleSummary(id.getSpecification().getId(), id.getClause(),
-                String.valueOf(id.getTestNumber()), status, totalChecks,
-                description, new HashSet<Check>());
+        if (assertions == null) {
+            throw new NullPointerException(
+                    "Argument assertions can not be null");
+        }
+        Set<Check> checks = new HashSet<>();
+        Status status = Status.PASSED;
+        int passedChecks = 0;
+        int failedChecks = 0;
+        for (TestAssertion assertion : assertions) {
+            if (assertion.getStatus() == Status.PASSED) {
+                passedChecks++;
+                if (logPassedChecks)
+                    checks.add(Check.fromValue(assertion));
+            } else {
+                status = assertion.getStatus();
+                failedChecks++;
+                checks.add(Check.fromValue(assertion));
+            }
+        }
+        return new RuleSummary(id, status, passedChecks, failedChecks,
+                description, checks);
     }
 
-    boolean addCheck(Check check) {
-        if (check == null) {
-            throw new IllegalArgumentException("Argument check con not be null");
+    static RuleSummary uncheckedInstance(final RuleId id,
+            final String description) {
+        if (id == null) {
+            throw new NullPointerException("Argument id can not be null");
         }
-        return this.checks.add(check);
-    }
-
-    void setStatus(TestAssertion.Status status) {
-        this.status = status;
+        if (description == null) {
+            throw new NullPointerException(
+                    "Argument description can not be null");
+        }
+        return new RuleSummary(id, Status.PASSED, description);
     }
 }
