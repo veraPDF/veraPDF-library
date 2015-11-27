68a3428e8dd74856b9b61fdc959b8a2e01347907
Carl Wilson <carl@openplanetsfoundation.org>
2015-11-27 18:14:15 +0000

    Made HTML report conversion stream based.

--- gui/src/main/java/org/verapdf/report/HTMLReport.java
+++ gui/src/main/java/org/verapdf/report/HTMLReport.java
@@ -1,14 +1,16 @@
 package org.verapdf.report;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import javax.xml.bind.JAXBException;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
-import java.io.File;
-import java.io.IOException;
-import java.io.OutputStream;
 
 /**
  * Generating HTML validation report
@@ -17,25 +19,37 @@ import java.io.OutputStream;
  */
 public final class HTMLReport {
 
-	private HTMLReport() {
-	}
+    private HTMLReport() {
+    }
 
-	/**
-	 * Creates html validation report
-	 * @param report
-	 * @param destination 
-	 *
-	 * @throws TransformerException if an unrecoverable error occurs during the course of the transformation or
-	 * @throws IOException          file system exceptions
-	 * @throws JAXBException 
-	 */
-	public static void writeHTMLReport(File report, OutputStream destination) throws TransformerException, IOException, JAXBException {
+    /**
+     * Creates html validation report
+     * 
+     * @param source
+     *            an {@link InputStream} instance that is the source Machine
+     *            Readable Report.
+     * @param destination
+     *            an {@link OutputStream} to write the HTML report to.
+     *
+     * @throws TransformerException
+     *             if an unrecoverable error occurs during the course of the
+     *             transformation
+     * @throws IOException
+     *             file system exceptions
+     * @throws JAXBException
+     */
+    public static void writeHTMLReport(InputStream source,
+            OutputStream destination) throws TransformerException, IOException,
+            JAXBException {
 
-		TransformerFactory factory = TransformerFactory.newInstance();
+        TransformerFactory factory = TransformerFactory.newInstance();
 
-		Transformer transformer = factory.newTransformer(new StreamSource(HTMLReport.class.getClassLoader().getResourceAsStream("HTMLReportStylesheet.xsl")));
+        Transformer transformer = factory.newTransformer(new StreamSource(
+                HTMLReport.class.getClassLoader().getResourceAsStream(
+                        "HTMLReportStylesheet.xsl")));
 
-		transformer.transform(new StreamSource(report), new StreamResult(destination));
-	}
+        transformer.transform(new StreamSource(source), new StreamResult(
+                destination));
+    }
 
 }
