6ca05d56499130c1ed8f511ab4078dc6d6536c3e
Carl Wilson <carl@openplanetsfoundation.org>
2015-11-30 11:48:25 +0000

    Developing CLI parser tests.

---
+++ gui/src/test/java/org/verapdf/cli/commands/VeraCliArgParserTest.java
@@ -0,0 +1,88 @@
+/**
+ * 
+ */
+package org.verapdf.cli.commands;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author <a href="mailto:carl@openpreservation.org">Carl Wilson</a>
+ *
+ */
+public class VeraCliArgParserTest {
+
+    /**
+     * Test method for {@link org.verapdf.cli.commands.VeraCliArgParser#showVersion()}.
+     */
+    @Test
+    public final void testShowVersion() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link org.verapdf.cli.commands.VeraCliArgParser#listProfiles()}.
+     */
+    @Test
+    public final void testListProfiles() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link org.verapdf.cli.commands.VeraCliArgParser#isHelp()}.
+     */
+    @Test
+    public final void testIsHelp() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link org.verapdf.cli.commands.VeraCliArgParser#getFormat()}.
+     */
+    @Test
+    public final void testGetFormat() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link org.verapdf.cli.commands.VeraCliArgParser#logPassed()}.
+     */
+    @Test
+    public final void testLogPassed() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link org.verapdf.cli.commands.VeraCliArgParser#extractFeatures()}.
+     */
+    @Test
+    public final void testExtractFeatures() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link org.verapdf.cli.commands.VeraCliArgParser#getFlavour()}.
+     */
+    @Test
+    public final void testGetFlavour() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link org.verapdf.cli.commands.VeraCliArgParser#getProfile()}.
+     */
+    @Test
+    public final void testGetProfile() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link org.verapdf.cli.commands.VeraCliArgParser#getPathsToValidate()}.
+     */
+    @Test
+    public final void testGetPathsToValidate() {
+        fail("Not yet implemented"); // TODO
+    }
+
+}
