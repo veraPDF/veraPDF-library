6b733a8db2f21d0c15f3fdc9f9437f2aabfa9bc1
Maksim Bezrukov <bezrukovm95@gmail.com>
2015-12-02 11:02:34 +0300

    Added JUnit test for metadata pdf identification fix.

--- metadata-fixer/src/test/java/org/verapdf/metadata/fixer/impl/pb/model/MetadataImplTest.java
+++ metadata-fixer/src/test/java/org/verapdf/metadata/fixer/impl/pb/model/MetadataImplTest.java
@@ -1,14 +1,5 @@
 package org.verapdf.metadata.fixer.impl.pb.model;
 
-import static org.junit.Assert.assertEquals;
-
-import java.io.File;
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 import org.apache.pdfbox.cos.COSStream;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.common.PDMetadata;
@@ -17,19 +8,49 @@ import org.apache.xmpbox.schema.PDFAIdentificationSchema;
 import org.apache.xmpbox.xml.DomXmpParser;
 import org.apache.xmpbox.xml.XmpParsingException;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.verapdf.pdfa.flavours.PDFAFlavour;
 import org.verapdf.pdfa.results.MetadataFixerResultImpl;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * @author Maksim Bezrukov
  */
-@SuppressWarnings({ "static-method", "javadoc" })
+@SuppressWarnings({"static-method", "javadoc"})
+@RunWith(Parameterized.class)
 public class MetadataImplTest {
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {"/test1.pdf", 1, "B"}, {"/test2.pdf", 1, "B"}
+        });
+    }
+
+    @Parameterized.Parameter
+    public String filePath;
+
+    @Parameterized.Parameter(value = 1)
+    public Integer filePart;
+
+    @Parameterized.Parameter(value = 2)
+    public String fileConformance;
 
     @Test
     public void addPDFIdentificationSchemaTest() throws URISyntaxException,
             IOException, XmpParsingException {
-        File pdf = new File(getSystemIndependentPath("/test.pdf"));
+        File pdf = new File(getSystemIndependentPath(filePath));
         try (PDDocument doc = PDDocument.load(pdf, false, true)) {
             PDMetadata meta = doc.getDocumentCatalog().getMetadata();
             DomXmpParser xmpParser = new DomXmpParser();
@@ -43,8 +64,8 @@ public class MetadataImplTest {
                 PDFAIdentificationSchema idCopy = xmp
                         .getPDFIdentificationSchema();
 
-                assertEquals(Integer.valueOf(1), idCopy.getPart());
-                assertEquals("B", idCopy.getConformance());
+                assertEquals(filePart, idCopy.getPart());
+                assertEquals(fileConformance, idCopy.getConformance());
             }
         }
     }
