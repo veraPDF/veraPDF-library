mass1ve-err0r/veraPDF-library-x

Arlington. Update reports wording

core/src/main/java/org/verapdf/pdfa/results/ValidationResultImpl.java
@@ -41,7 +41,7 @@ import java.util.*;
 /**
  * @author <a href="mailto:carl@openpreservation.org">Carl Wilson</a>
  */
-@XmlRootElement(name = "validationResult")
+@XmlRootElement(name = "arlingtonPDFModelCheckingResult")
 final class ValidationResultImpl implements ValidationResult {
 	private static final ValidationResultImpl DEFAULT = new ValidationResultImpl();
 	@XmlAttribute

core/src/main/java/org/verapdf/processor/reports/BatchSummaryImpl.java
@@ -33,7 +33,7 @@ import org.verapdf.component.Components;
 @XmlRootElement(name = "batchSummary")
 final class BatchSummaryImpl implements BatchSummary {
 	private static final BatchSummary DEFAULT = new BatchSummaryImpl();
-	@XmlElement
+	@XmlElement(name="arlingtonPDFModelCheckingReports")
 	private final ValidationBatchSummary validationReports;
 	@XmlElement
 	private final FeaturesBatchSummary featureReports;

core/src/main/java/org/verapdf/processor/reports/ValidationBatchSummaryImpl.java
@@ -4,7 +4,7 @@ import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.adapters.XmlAdapter;
 
-@XmlRootElement(name = "validationReports")
+@XmlRootElement(name = "arlingtonPDFModelCheckingReports")
 final class ValidationBatchSummaryImpl extends AbstractBatchJobSummary implements ValidationBatchSummary {
 	static final ValidationBatchSummary DEFAULT = new ValidationBatchSummaryImpl();
 	@XmlAttribute

core/src/main/java/org/verapdf/processor/reports/ValidationReportImpl.java
@@ -35,7 +35,7 @@ import javax.xml.bind.annotation.adapters.XmlAdapter;
  *         <a href="https://github.com/carlwilson">carlwilson AT github</a>
  * @version 0.1 Created 10 Nov 2016:08:18:17
  */
-@XmlRootElement(name = "validationReport")
+@XmlRootElement(name = "arlingtonPDFModelCheckingReport")
 final class ValidationReportImpl implements ValidationReport {
 	private static final ValidationReportImpl defaultInstance = new ValidationReportImpl();
 	@XmlElement

core/src/main/java/org/verapdf/processor/reports/multithread/writer/ReportParserEventHandler.java
@@ -16,7 +16,7 @@ public class ReportParserEventHandler extends DefaultHandler {
     private static final Logger LOGGER = Logger.getLogger(ReportParserEventHandler.class.getCanonicalName());
 
     private static final Set<String> BATCH_SUMMARY_TAGS =
-            new HashSet<>(Arrays.asList("batchSummary", "validationReports", "featureReports", "repairReports"));
+            new HashSet<>(Arrays.asList("batchSummary", "arlingtonPDFModelCheckingReports", "featureReports", "repairReports"));
 
     private String element;
     private final Map<String, Map<String, Integer>> batchSummary = new LinkedHashMap<>();

core/src/main/resources/org/verapdf/report/DetailedHtmlReport.xsl
@@ -92,7 +92,7 @@
             <xsl:variable name="validClass">
                 <xsl:choose>
                     <xsl:when
-                            test="/report/jobs/job/validationReport/@isCompliant = 'true'">
+                            test="/report/jobs/job/arlingtonPDFModelCheckingReport/@isCompliant = 'true'">
                         <xsl:value-of select="'valid'"/>
                     </xsl:when>
                     <xsl:otherwise>
@@ -110,27 +110,27 @@
                         <xsl:value-of select="/report/jobs/job/item/name"/>
                     </td>
                 </tr>
-                <xsl:if test="/report/jobs/job/validationReport/@profileName">
+                <xsl:if test="/report/jobs/job/arlingtonPDFModelCheckingReport/@profileName">
                     <tr>
                         <td width="200">
                             <b>Profile:</b>
                         </td>
                         <td>
                             <xsl:value-of
-                                    select="/report/jobs/job/validationReport/@profileName"/>
+                                    select="/report/jobs/job/arlingtonPDFModelCheckingReport/@profileName"/>
                         </td>
                     </tr>
                 </xsl:if>
-<!--                <xsl:if test="/report/jobs/job/validationReport/@isCompliant">-->
+<!--                <xsl:if test="/report/jobs/job/arlingtonPDFModelCheckingReport/@isCompliant">-->
 <!--                    <tr>-->
 <!--                        <td width="200" class="{$validClass}">-->
 <!--                            Compliance:-->
 <!--                        </td>-->
 <!--                        <td class="{$validClass}">-->
-<!--                            <xsl:if test="/report/jobs/job/validationReport/@isCompliant = 'true'">-->
+<!--                            <xsl:if test="/report/jobs/job/arlingtonPDFModelCheckingReport/@isCompliant = 'true'">-->
 <!--                                Passed-->
 <!--                            </xsl:if>-->
-<!--                            <xsl:if test="/report/jobs/job/validationReport/@isCompliant = 'false'">-->
+<!--                            <xsl:if test="/report/jobs/job/arlingtonPDFModelCheckingReport/@isCompliant = 'false'">-->
 <!--                                Failed-->
 <!--                            </xsl:if>-->
 <!--                        </td>-->
@@ -201,36 +201,36 @@
                         <xsl:value-of select="/report/jobs/job/duration"/>
                     </td>
                 </tr>
-                <xsl:if test="/report/jobs/job/validationReport/details/@passedRules or /report/jobs/job/validationReport/details/@failedRules">
+                <xsl:if test="/report/jobs/job/arlingtonPDFModelCheckingReport/details/@passedRules or /report/jobs/job/arlingtonPDFModelCheckingReport/details/@failedRules">
                     <tr>
                         <td width="250">
                             <b>Total rules in Profile:</b>
                         </td>
                         <td>
                             <xsl:value-of
-                                    select="/report/jobs/job/validationReport/details/@passedRules + /report/jobs/job/validationReport/details/@failedRules"/>
+                                    select="/report/jobs/job/arlingtonPDFModelCheckingReport/details/@passedRules + /report/jobs/job/arlingtonPDFModelCheckingReport/details/@failedRules"/>
                         </td>
                     </tr>
                 </xsl:if>
-                <xsl:if test="/report/jobs/job/validationReport/details/@passedChecks">
+                <xsl:if test="/report/jobs/job/arlingtonPDFModelCheckingReport/details/@passedChecks">
                     <tr>
                         <td width="250">
                             <b>Checks:</b>
                         </td>
                         <td>
                             <xsl:value-of
-                                    select="/report/jobs/job/validationReport/details/@passedChecks"/>
+                                    select="/report/jobs/job/arlingtonPDFModelCheckingReport/details/@passedChecks"/>
                         </td>
                     </tr>
                 </xsl:if>
-                <xsl:if test="/report/jobs/job/validationReport/details/@failedChecks">
+                <xsl:if test="/report/jobs/job/arlingtonPDFModelCheckingReport/details/@failedChecks">
                     <tr>
                         <td width="250">
                             <b>Deviations:</b>
                         </td>
                         <td>
                             <xsl:value-of
-                                    select="/report/jobs/job/validationReport/details/@failedChecks"/>
+                                    select="/report/jobs/job/arlingtonPDFModelCheckingReport/details/@failedChecks"/>
                         </td>
                     </tr>
                 </xsl:if>
@@ -300,7 +300,7 @@
                 </table>
             </xsl:if>
 
-            <xsl:if test="/report/jobs/job/validationReport/details/rule">
+            <xsl:if test="/report/jobs/job/arlingtonPDFModelCheckingReport/details/rule">
                 <h2>Analysis messages</h2>
 
                 <table border="0" id="table3">
@@ -313,7 +313,7 @@
                         </td>
                     </tr>
                     <xsl:apply-templates
-                            select="/report/jobs/job/validationReport/details/rule"/>
+                            select="/report/jobs/job/arlingtonPDFModelCheckingReport/details/rule"/>
                 </table>
             </xsl:if>
 
@@ -397,7 +397,7 @@
     </xsl:template>
 
     <!-- Validation Information -->
-    <xsl:template match="/report/jobs/job/validationReport/details/rule">
+    <xsl:template match="/report/jobs/job/arlingtonPDFModelCheckingReport/details/rule">
 
         <xsl:param name="idWithDots" select="concat(@clause, 't', @testNumber)"/>
         <xsl:param name="id" select="translate($idWithDots, '.', '_')"/>

core/src/main/resources/org/verapdf/report/SummaryHtmlReport.xsl
@@ -176,10 +176,10 @@
                   <xsl:value-of select="@encrypted"/>
               </td>
               <td>
-                  <xsl:value-of select="validationReports/@compliant"/>
+                  <xsl:value-of select="arlingtonPDFModelCheckingReports/@compliant"/>
               </td>
               <td>
-                  <xsl:value-of select="validationReports/@nonCompliant"/>
+                  <xsl:value-of select="arlingtonPDFModelCheckingReports/@nonCompliant"/>
               </td>
               <xsl:if test="$isPolicy">
                 <td>

veraPDF/veraPDF-library

core/src/main/java/org/verapdf/pdfa/results/ValidationResultImpl.java
@@ -41,7 +41,7 @@ import java.util.*;
 /**
  * @author <a href="mailto:carl@openpreservation.org">Carl Wilson</a>
  */
-@XmlRootElement(name = "validationResult")
+@XmlRootElement(name = "arlingtonResult")
 final class ValidationResultImpl implements ValidationResult {
 	private static final ValidationResultImpl DEFAULT = new ValidationResultImpl();
 	@XmlAttribute

core/src/main/java/org/verapdf/processor/reports/BatchSummaryImpl.java
@@ -33,7 +33,7 @@ import org.verapdf.component.Components;
 @XmlRootElement(name = "batchSummary")
 final class BatchSummaryImpl implements BatchSummary {
 	private static final BatchSummary DEFAULT = new BatchSummaryImpl();
-	@XmlElement
+	@XmlElement(name="arlingtonReports")
 	private final ValidationBatchSummary validationReports;
 	@XmlElement
 	private final FeaturesBatchSummary featureReports;

core/src/main/java/org/verapdf/processor/reports/ValidationBatchSummaryImpl.java
@@ -4,7 +4,7 @@ import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.adapters.XmlAdapter;
 
-@XmlRootElement(name = "validationReports")
+@XmlRootElement(name = "arlingtonReports")
 final class ValidationBatchSummaryImpl extends AbstractBatchJobSummary implements ValidationBatchSummary {
 	static final ValidationBatchSummary DEFAULT = new ValidationBatchSummaryImpl();
 	@XmlAttribute

core/src/main/java/org/verapdf/processor/reports/ValidationReportImpl.java
@@ -35,7 +35,7 @@ import javax.xml.bind.annotation.adapters.XmlAdapter;
  *         <a href="https://github.com/carlwilson">carlwilson AT github</a>
  * @version 0.1 Created 10 Nov 2016:08:18:17
  */
-@XmlRootElement(name = "validationReport")
+@XmlRootElement(name = "arlingtonReport")
 final class ValidationReportImpl implements ValidationReport {
 	private static final ValidationReportImpl defaultInstance = new ValidationReportImpl();
 	@XmlElement

core/src/main/java/org/verapdf/processor/reports/multithread/writer/ReportParserEventHandler.java
@@ -16,7 +16,7 @@ public class ReportParserEventHandler extends DefaultHandler {
     private static final Logger LOGGER = Logger.getLogger(ReportParserEventHandler.class.getCanonicalName());
 
     private static final Set<String> BATCH_SUMMARY_TAGS =
-            new HashSet<>(Arrays.asList("batchSummary", "validationReports", "featureReports", "repairReports"));
+            new HashSet<>(Arrays.asList("batchSummary", "arlingtonReports", "featureReports", "repairReports"));
 
     private String element;
     private final Map<String, Map<String, Integer>> batchSummary = new LinkedHashMap<>();

core/src/main/resources/org/verapdf/report/DetailedHtmlReport.xsl
@@ -92,7 +92,7 @@
             <xsl:variable name="validClass">
                 <xsl:choose>
                     <xsl:when
-                            test="/report/jobs/job/validationReport/@isCompliant = 'true'">
+                            test="/report/jobs/job/arlingtonReport/@isCompliant = 'true'">
                         <xsl:value-of select="'valid'"/>
                     </xsl:when>
                     <xsl:otherwise>
@@ -110,27 +110,27 @@
                         <xsl:value-of select="/report/jobs/job/item/name"/>
                     </td>
                 </tr>
-                <xsl:if test="/report/jobs/job/validationReport/@profileName">
+                <xsl:if test="/report/jobs/job/arlingtonReport/@profileName">
                     <tr>
                         <td width="200">
                             <b>Profile:</b>
                         </td>
                         <td>
                             <xsl:value-of
-                                    select="/report/jobs/job/validationReport/@profileName"/>
+                                    select="/report/jobs/job/arlingtonReport/@profileName"/>
                         </td>
                     </tr>
                 </xsl:if>
-<!--                <xsl:if test="/report/jobs/job/validationReport/@isCompliant">-->
+<!--                <xsl:if test="/report/jobs/job/arlingtonReport/@isCompliant">-->
 <!--                    <tr>-->
 <!--                        <td width="200" class="{$validClass}">-->
 <!--                            Compliance:-->
 <!--                        </td>-->
 <!--                        <td class="{$validClass}">-->
-<!--                            <xsl:if test="/report/jobs/job/validationReport/@isCompliant = 'true'">-->
+<!--                            <xsl:if test="/report/jobs/job/arlingtonReport/@isCompliant = 'true'">-->
 <!--                                Passed-->
 <!--                            </xsl:if>-->
-<!--                            <xsl:if test="/report/jobs/job/validationReport/@isCompliant = 'false'">-->
+<!--                            <xsl:if test="/report/jobs/job/arlingtonReport/@isCompliant = 'false'">-->
 <!--                                Failed-->
 <!--                            </xsl:if>-->
 <!--                        </td>-->
@@ -201,36 +201,36 @@
                         <xsl:value-of select="/report/jobs/job/duration"/>
                     </td>
                 </tr>
-                <xsl:if test="/report/jobs/job/validationReport/details/@passedRules or /report/jobs/job/validationReport/details/@failedRules">
+                <xsl:if test="/report/jobs/job/arlingtonReport/details/@passedRules or /report/jobs/job/arlingtonReport/details/@failedRules">
                     <tr>
                         <td width="250">
                             <b>Total rules in Profile:</b>
                         </td>
                         <td>
                             <xsl:value-of
-                                    select="/report/jobs/job/validationReport/details/@passedRules + /report/jobs/job/validationReport/details/@failedRules"/>
+                                    select="/report/jobs/job/arlingtonReport/details/@passedRules + /report/jobs/job/arlingtonReport/details/@failedRules"/>
                         </td>
                     </tr>
                 </xsl:if>
-                <xsl:if test="/report/jobs/job/validationReport/details/@passedChecks">
+                <xsl:if test="/report/jobs/job/arlingtonReport/details/@passedChecks">
                     <tr>
                         <td width="250">
                             <b>Checks:</b>
                         </td>
                         <td>
                             <xsl:value-of
-                                    select="/report/jobs/job/validationReport/details/@passedChecks"/>
+                                    select="/report/jobs/job/arlingtonReport/details/@passedChecks"/>
                         </td>
                     </tr>
                 </xsl:if>
-                <xsl:if test="/report/jobs/job/validationReport/details/@failedChecks">
+                <xsl:if test="/report/jobs/job/arlingtonReport/details/@failedChecks">
                     <tr>
                         <td width="250">
                             <b>Deviations:</b>
                         </td>
                         <td>
                             <xsl:value-of
-                                    select="/report/jobs/job/validationReport/details/@failedChecks"/>
+                                    select="/report/jobs/job/arlingtonReport/details/@failedChecks"/>
                         </td>
                     </tr>
                 </xsl:if>
@@ -300,7 +300,7 @@
                 </table>
             </xsl:if>
 
-            <xsl:if test="/report/jobs/job/validationReport/details/rule">
+            <xsl:if test="/report/jobs/job/arlingtonReport/details/rule">
                 <h2>Analysis messages</h2>
 
                 <table border="0" id="table3">
@@ -313,7 +313,7 @@
                         </td>
                     </tr>
                     <xsl:apply-templates
-                            select="/report/jobs/job/validationReport/details/rule"/>
+                            select="/report/jobs/job/arlingtonReport/details/rule"/>
                 </table>
             </xsl:if>
 
@@ -397,7 +397,7 @@
     </xsl:template>
 
     <!-- Validation Information -->
-    <xsl:template match="/report/jobs/job/validationReport/details/rule">
+    <xsl:template match="/report/jobs/job/arlingtonReport/details/rule">
 
         <xsl:param name="idWithDots" select="concat(@clause, 't', @testNumber)"/>
         <xsl:param name="id" select="translate($idWithDots, '.', '_')"/>

core/src/main/resources/org/verapdf/report/SummaryHtmlReport.xsl
@@ -176,10 +176,10 @@
                   <xsl:value-of select="@encrypted"/>
               </td>
               <td>
-                  <xsl:value-of select="validationReports/@compliant"/>
+                  <xsl:value-of select="arlingtonReports/@compliant"/>
               </td>
               <td>
-                  <xsl:value-of select="validationReports/@nonCompliant"/>
+                  <xsl:value-of select="arlingtonReports/@nonCompliant"/>
               </td>
               <xsl:if test="$isPolicy">
                 <td>