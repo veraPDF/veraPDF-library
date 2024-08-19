c9f4351ceaca6a0be189c59082217a50e988d97a
Nikita Kovaliov <nikita.kovaliov@duallab.com>
2024-08-19 14:05:41 +0300

    Complete javadocs documentation for some interfaces

--- core/src/main/java/org/verapdf/features/objects/AnnotationFeaturesObjectAdapter.java
+++ core/src/main/java/org/verapdf/features/objects/AnnotationFeaturesObjectAdapter.java
@@ -26,23 +26,137 @@ import java.util.Set;
  * @author Maksim Bezrukov
  */
 public interface AnnotationFeaturesObjectAdapter extends FeaturesObjectAdapter {
+
+	/**
+	 * Gets annotation adapter id.
+	 *
+	 * @return an annotation adapter id
+	 */
 	String getId();
+
+	/**
+	 * Gets annotation adapter popup id.
+	 *
+	 * @return an annotation adapter popup id
+	 */
 	String getPopupId();
+
+	/**
+	 * Gets form XObject from annotation.
+	 *
+	 * @return a form XObject
+	 */
 	Set<String> getFormXObjectsResources();
+
+	/**
+	 * Gets annotation subtype.
+	 *
+	 * @return an annotation subtype
+	 */
 	String getSubtype();
+
+	/**
+	 * Gets annotation rectangle.
+	 *
+	 * @return an annotation rectangle
+	 */
 	double[] getRectangle();
+
+	/**
+	 * Gets annotation contents.
+	 *
+	 * @return an annotation contents
+	 */
 	String getContents();
+
+	/**
+	 * Gets annotation name.
+	 *
+	 * @return an annotation name
+	 */
 	String getAnnotationName();
+
+	/**
+	 * Gets annotation modified date.
+	 *
+	 * @return an annotation modified date
+	 */
 	String getModifiedDate();
+
+	/**
+	 * Gets annotation color.
+	 *
+	 * @return an annotation color
+	 */
 	double[] getColor();
+
+	/**
+	 * Tells whether annotation is invisible.
+	 *
+	 * @return true if annotation is invisible
+	 */
 	boolean isInvisible();
+
+	/**
+	 * Tells whether annotation is hidden.
+	 *
+	 * @return true if annotation is hidden
+	 */
 	boolean isHidden();
+
+	/**
+	 * Tells whether annotation is printed.
+	 *
+	 * @return true if annotation is printed
+	 */
 	boolean isPrinted();
+
+	/**
+	 * Tells whether annotation is no zoom.
+	 *
+	 * @return true if annotation is no zoom
+	 */
 	boolean isNoZoom();
+
+	/**
+	 * Tells whether annotation is no rotate.
+	 *
+	 * @return true if annotation is no rotate
+	 */
 	boolean isNoRotate();
+
+	/**
+	 * Tells whether annotation is no view.
+	 *
+	 * @return true if annotation is no view
+	 */
 	boolean isNoView();
+
+	/**
+	 * Tells whether annotation is read only.
+	 *
+	 * @return true if annotation is read only
+	 */
 	boolean isReadOnly();
+
+	/**
+	 * Tells whether annotation is locked.
+	 *
+	 * @return true if annotation is locked
+	 */
 	boolean isLocked();
+
+	/**
+	 * Tells whether annotation is toggle no view.
+	 *
+	 * @return true if annotation is toggle no view
+	 */
 	boolean isToggleNoView();
+
+	/**
+	 * Tells whether annotation is locked content.
+	 *
+	 * @return true if annotation is locked content
+	 */
 	boolean isLockedContents();
 }
