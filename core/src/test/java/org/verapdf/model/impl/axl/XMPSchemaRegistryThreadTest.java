baf27bdd60c1881c1e89947629444e11af89f752
Sai Asish Y <say.apm35@gmail.com>
2026-06-02 13:40:00 -0700

    fix: self-initialise XMP container thread-locals to avoid NPE on foreign threads
    Signed-off-by: Sai Asish Y <say.apm35@gmail.com>

---
+++ core/src/test/java/org/verapdf/model/impl/axl/XMPSchemaRegistryThreadTest.java
@@ -0,0 +1,55 @@
+/*
+ * This file is part of veraPDF Library core, a module of the veraPDF project.
+ * Copyright (c) 2015-2026, veraPDF Consortium <info@verapdf.org>
+ * All rights reserved.
+ *
+ * veraPDF Library core is free software: you can redistribute it and/or modify
+ * it under the terms of either:
+ *
+ * The GNU General public license GPLv3+.
+ * You should have received a copy of the GNU General Public License
+ * along with veraPDF Library core as the LICENSE.GPL file in the root of the source
+ * tree.  If not, see http://www.gnu.org/licenses/ or
+ * https://www.gnu.org/licenses/gpl-3.0.en.html.
+ *
+ * The Mozilla Public License MPLv2+.
+ * You should have received a copy of the Mozilla Public License along with
+ * veraPDF Library core as the LICENSE.MPL file in the root of the source tree.
+ * If a copy of the MPL was not distributed with this file, you can obtain one at
+ * http://mozilla.org/MPL/2.0/.
+ */
+package org.verapdf.model.impl.axl;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+import org.junit.Test;
+import org.verapdf.xmp.XMPConst;
+import org.verapdf.xmp.XMPMetaFactory;
+
+public class XMPSchemaRegistryThreadTest {
+
+    @Test
+    public void getNamespacePrefixFromForeignThread() throws InterruptedException {
+        // The singleton registry is constructed on this thread, which seeds the
+        // per-thread containers only here. Another thread (such as a virtual
+        // thread) has its own empty container slots.
+        XMPMetaFactory.getSchemaRegistry();
+
+        AtomicReference<String> prefix = new AtomicReference<>();
+        AtomicReference<Throwable> failure = new AtomicReference<>();
+        Thread worker = new Thread(() -> {
+            try {
+                prefix.set(XMPMetaFactory.getSchemaRegistry().getNamespacePrefix(XMPConst.NS_RDF));
+            } catch (Throwable t) {
+                failure.set(t);
+            }
+        });
+        worker.start();
+        worker.join();
+
+        assertEquals(null, failure.get());
+        assertEquals("rdf:", prefix.get());
+    }
+}
